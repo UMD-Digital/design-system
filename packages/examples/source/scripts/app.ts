01b94cf231606b50dde3177adc58f35181313f7f
Josh Magness <magnessjo@gmail.com>
2023-10-29 14:29:31 -0400

    Refactor alerts scripts to testing function

--- packages/examples/source/scripts/app.ts
+++ packages/examples/source/scripts/app.ts
@@ -1,38 +1,7 @@
 require('styles/index.css');
 import '@universityofmaryland/umd-web-components';
-
-const Alerts = () => {
-  const elements = Array.from(document.querySelectorAll('umd-element-alert'));
-
-  const observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      if (mutation.type === 'attributes') {
-        if (mutation.attributeName === 'closed') {
-          console.log('closed callback received');
-        }
-      }
-    });
-  });
-
-  elements.forEach((element) => {
-    observer.observe(element, { attributes: true });
-
-    setTimeout(() => {
-      element.setAttribute('type', 'alert');
-      element.setAttribute('icon', 'false');
-    }, 2000);
-
-    setTimeout(() => {
-      element.setAttribute('type', 'emergency');
-      element.setAttribute('icon', 'true');
-    }, 4000);
-
-    setTimeout(() => {
-      element.setAttribute('type', 'notification');
-    }, 6000);
-  });
-};
+import { AlertsTest } from './elements/alerts';
 
 document.addEventListener('DOMContentLoaded', () => {
-  Alerts();
+  AlertsTest();
 });
---
+++ packages/examples/source/scripts/elements/alerts.ts
@@ -0,0 +1,31 @@
+export const AlertsTest = () => {
+  const elements = Array.from(document.querySelectorAll('umd-element-alert'));
+
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.type === 'attributes') {
+        if (mutation.attributeName === 'closed') {
+          console.log('closed callback received');
+        }
+      }
+    });
+  });
+
+  elements.forEach((element) => {
+    observer.observe(element, { attributes: true });
+
+    setTimeout(() => {
+      element.setAttribute('type', 'alert');
+      element.setAttribute('icon', 'false');
+    }, 2000);
+
+    setTimeout(() => {
+      element.setAttribute('type', 'emergency');
+      element.setAttribute('icon', 'true');
+    }, 4000);
+
+    setTimeout(() => {
+      element.setAttribute('type', 'notification');
+    }, 6000);
+  });
+};

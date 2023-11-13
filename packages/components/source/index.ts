884ff1aa8510460eb0cfd8461c9d6b72b1bb2bdd
Josh Magness <magnessjo@gmail.com>
2023-11-13 09:38:01 -0500

    Card Loader for all web components

--- packages/components/source/index.ts
+++ packages/components/source/index.ts
@@ -1,11 +1,13 @@
 import { MakeDefaultStyleTag } from 'helpers/ui';
 import { Load as AlertLoader } from './components/alert/component';
+import { Load as CardLoader } from './components/card/component';
 import { Load as CarouselCardsLoader } from './components/carousel-cards/component';
 import { Load as DateSliderLoader } from './components/events-date-slider/component';
 import { Load as FooterLoader } from './components/footer/component';
 import { Load as NavDropdownLoader } from './components/nav-item/component';
 
 let styleString = AlertLoader();
+styleString += CardLoader();
 styleString += CarouselCardsLoader();
 styleString += DateSliderLoader();
 styleString += FooterLoader();

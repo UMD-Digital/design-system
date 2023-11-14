efaef5ba134f4267946b654e9f09c8d76e095180
Josh Magness <magnessjo@gmail.com>
2023-11-14 09:46:47 -0500

    Card Overlay Background Image Example

--- packages/components/source/components/card-overlay/elements/cta.ts
+++ packages/components/source/components/card-overlay/elements/cta.ts
@@ -1,12 +1,10 @@
 import { spacing } from '@universityofmaryland/umd-web-configuration/dist/tokens/layout.js';
 import { MakeSlot } from 'helpers/ui';
-import { SLOTS } from '../globals';
+import { SLOTS, ELEMENTS } from '../globals';
 import { CardType } from '../component';
 
-const CARD_OVERLAY_CONTAINER_CTA = 'umd-card-overlay-container-cta';
-
 export const CtaStyles = `
-  .${CARD_OVERLAY_CONTAINER_CTA} {
+  .${ELEMENTS.CARD_OVERLAY_CONTAINER_CTA} {
     position: absolute;
     bottom: ${spacing.md};
     left: ${spacing.md};
@@ -17,7 +15,7 @@ export const CreateCta = ({ element }: { element: CardType }) => {
   const container = document.createElement('div');
   const ctaSlot = MakeSlot({ type: SLOTS.CTA });
 
-  container.classList.add(CARD_OVERLAY_CONTAINER_CTA);
+  container.classList.add(ELEMENTS.CARD_OVERLAY_CONTAINER_CTA);
 
   container.appendChild(ctaSlot);
 

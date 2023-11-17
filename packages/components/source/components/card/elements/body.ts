2c06a6b3c57778b14aba6081fb4caac9046fd720
Josh Magness <magnessjo@gmail.com>
2023-11-17 12:47:58 -0500

    Card Text Spacing
    fixes DSYS-204

--- packages/components/source/components/card/elements/body.ts
+++ packages/components/source/components/card/elements/body.ts
@@ -1,13 +1,21 @@
+import { spacing } from '@universityofmaryland/umd-web-configuration/dist/tokens/layout.js';
 import { MakeSlot } from 'helpers/ui';
 import { SLOTS } from '../globals';
 import { CardType } from '../component';
 
-export const BodyStyles = ``;
+const CARD_BODY_CONTAINER = 'umd-card-body-container';
+
+export const BodyStyles = `
+  * + .${CARD_BODY_CONTAINER} {
+    margin-top: ${spacing.min};
+  }
+`;
 
 export const CreateBody = ({ element }: { element: CardType }) => {
   const container = document.createElement('div');
   const contentSlot = MakeSlot({ type: SLOTS.BODY });
 
+  container.classList.add(CARD_BODY_CONTAINER);
   container.appendChild(contentSlot);
 
   return container;

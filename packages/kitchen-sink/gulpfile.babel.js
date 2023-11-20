fa75d54b4d124807e03f365231b2ffef92a02a9e
Josh Magness <magnessjo@gmail.com>
2023-11-20 09:10:21 -0500

    Ktichen Sink - Refactor twig structure

--- packages/kitchen-sink/gulpfile.babel.js
+++ packages/kitchen-sink/gulpfile.babel.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const twig = require('gulp-twig');
 
-const twigSource = 'source/twig/*.twig';
+const twigSource = 'source/twig/**/*.twig';
 const buildDir = './build';
 
 const twigTask = () =>

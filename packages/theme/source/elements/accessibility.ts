008dc1265c62513a2f1d0f6ccc2cac806f2bd80e
Max Escher <mescher@umd.edu>
2024-01-18 13:03:09 -0500

    Close DSYS-472: Refactor styles in theme package

---
+++ packages/theme/source/elements/accessibility.ts
@@ -0,0 +1,29 @@
+import { colors, spacing, typography } from '@universityofmaryland/variables';
+
+const skipContent = {
+  '.umd-skip-content': {
+    ...typography['.umd-interactive-sans-small'],
+    ...{
+      backgroundColor: colors.white,
+      color: colors.red,
+      display: 'block',
+      height: '0',
+      padding: spacing.min,
+      position: 'absolute',
+      zIndex: '-2147483647',
+      opacity: '0',
+      transition: 'none',
+      textDecoration: 'underline',
+
+      '&:focus': {
+        display: 'block',
+        height: 'inherit',
+        zIndex: '2147483647',
+        opacity: '1',
+        transition: 'opacity 0.5s',
+      },
+    },
+  },
+};
+
+export { skipContent };
--- packages/theme/source/index.ts
+++ packages/theme/source/index.ts
@@ -1,3 +1,48 @@
-const theme = {};
+import {
+  breakpoints,
+  queries,
+  colors,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  spacing,
+  screenReaderOnly,
+  animatedLinks,
+  typography,
+  umdFlexGrid,
+  umdGrid,
+  umdLock,
+} from '@universityofmaryland/variables';
 
-export { theme };
+import { root } from './dependancies/root';
+import { skipContent } from './elements/accessibility';
+import { umdCta } from './elements/call-to-action';
+import { umdLoader } from './elements/loader';
+import { captionedMedia } from './elements/media-with-caption';
+import { richText } from './elements/rich-text';
+import { umdLinksGroup } from './layout/umd-links-group';
+
+const theme = {
+  animatedLinks,
+  breakpoints,
+  captionedMedia,
+  colors,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  queries,
+  richText,
+  root,
+  spacing,
+  screenReaderOnly,
+  typography,
+  skipContent,
+  umdCta,
+  umdLoader,
+  umdFlexGrid,
+  umdGrid,
+  umdLinksGroup,
+  umdLock,
+};
+
+export default theme;

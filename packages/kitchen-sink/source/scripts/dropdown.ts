c3971ef951356e5a802ec981fb6e80889b0665df
Alia <apccello@umd.edu>
2024-02-05 10:20:01 -0500

    Close DSYS-504: Convert CTA to new UI

--- packages/kitchen-sink/source/scripts/dropdown.ts
+++ packages/kitchen-sink/source/scripts/dropdown.ts
@@ -15,6 +15,10 @@ const DropdownVariations = () => {
     });
   };
 
+  const triggerResize = (): void => {
+    window.dispatchEvent(new Event('resize'));
+  };
+
   const toggleContentVisibility = (selectedId: string): void => {
     hideAllContent();
 
@@ -22,6 +26,10 @@ const DropdownVariations = () => {
       document.getElementById(selectedId);
     if (selectedContentDiv) {
       selectedContentDiv.setAttribute('aria-hidden', 'false');
+
+      setTimeout(() => {
+        triggerResize();
+      }, 10);
     }
   };
 

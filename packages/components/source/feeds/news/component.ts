8c3f6cff2c8c6d93846ea677892a11b27b89dded
Josh Magness <magnessjo@gmail.com>
2024-02-06 09:59:43 -0500

    Feed component - Fetch data example

--- packages/components/source/feeds/news/component.ts
+++ packages/components/source/feeds/news/component.ts
@@ -7,14 +7,27 @@ declare global {
 import { MakeTemplate } from 'helpers/ui';
 import { ComponentStyles, CreateShadowDom } from './elements';
 
+const ATTRIBUTE_TOKEN = 'token';
+const ATTRIBUTE_ROWS = 'row-count';
+const ATTRIBUTE_SHOW = 'show-count';
+const ATTRIBUTE_LAZYLOAD = 'lazyload';
+
 export const ELEMENT_NAME = 'umd-feed-news';
-export type ExampleType = UMDFeedNews;
+export type UMDNewsFeedType = UMDFeedNews;
 export class UMDFeedNews extends HTMLElement {
   _shadow: ShadowRoot;
+  _token: string | null;
+  _showCount: number;
+  _showRows: number;
+  _lazyLoad: boolean;
 
   constructor() {
     super();
     this._shadow = this.attachShadow({ mode: 'open' });
+    this._token = null;
+    this._showCount = 3;
+    this._showRows = 1;
+    this._lazyLoad = false;
 
     const styles = `${ComponentStyles}`;
     const template = MakeTemplate({ styles });
@@ -23,14 +36,42 @@ export class UMDFeedNews extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return [];
+    return [
+      ATTRIBUTE_TOKEN,
+      ATTRIBUTE_ROWS,
+      ATTRIBUTE_LAZYLOAD,
+      ATTRIBUTE_SHOW,
+    ];
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {}
 
   connectedCallback() {
-    const container = CreateShadowDom({ element: this });
+    const element = this;
+    const rowCount = element.getAttribute(ATTRIBUTE_ROWS);
+    const showCount = element.getAttribute(ATTRIBUTE_SHOW);
+    const shouldLazyLoad = element.getAttribute(ATTRIBUTE_LAZYLOAD);
 
+    element._token = element.getAttribute(ATTRIBUTE_TOKEN) || null;
+
+    if (shouldLazyLoad) {
+      if (shouldLazyLoad === 'true') element._lazyLoad = true;
+      if (shouldLazyLoad === 'false') element._lazyLoad = false;
+    }
+
+    if (rowCount) {
+      if (rowCount === '1') element._showRows = 1;
+      if (rowCount === '2') element._showRows = 2;
+      if (rowCount === '3') element._showRows = 3;
+    }
+
+    if (showCount) {
+      if (showCount === '2') element._showCount = 1;
+      if (showCount === '3') element._showCount = 2;
+      if (showCount === '4') element._showCount = 3;
+    }
+
+    const container = CreateShadowDom({ element });
     this._shadow.appendChild(container);
   }
 }
--- packages/components/source/feeds/news/elements/index.ts
+++ packages/components/source/feeds/news/elements/index.ts
@@ -1,4 +1,55 @@
 import { Reset } from 'helpers/styles';
+import { FetchGraphQL } from 'helpers/xhr';
+import { UMDNewsFeedType } from '../component';
+
+type ArticleType = {
+  id: number;
+  title: string;
+  url: string;
+  summary: string;
+  image: {
+    url: string;
+    altText: string;
+  }[];
+  categories: {
+    title: string;
+    url: string;
+  }[];
+};
+
+type VariablesType = {
+  related?: string[];
+};
+
+const TODAY_PRODUCTION_URL = 'https://today.umd.edu/graphql';
+const ARTICLES_QUERY = `
+  query getArticlesByCategories($related: [QueryArgument]) {
+    entries(
+      section: "articles",
+      relatedTo: $related,
+      limit: 3
+    ) {
+      ... on articles_today_Entry {
+        id
+        title
+        postDate
+        summary: genericText
+        url
+        image:articlesHeroImage {
+          url
+          ... on hero_Asset {
+            id
+            altText: genericText
+          }
+        }
+        categories:categoryTodaySectionMultiple {
+          title
+          url
+        }
+      }
+    }
+  }
+`;
 
 export const ComponentStyles = `
   :host {
@@ -24,7 +75,20 @@ const CreateCallToAction = () => {
   return wrapper;
 };
 
-export const CreateShadowDom = ({ element }: { element: HTMLElement }) => {
+const CreateFeed = async ({ element }: { element: UMDNewsFeedType }) => {
+  if (!element._token) throw new Error('Token not found');
+
+  const feedData = await FetchGraphQL({
+    query: ARTICLES_QUERY,
+    url: TODAY_PRODUCTION_URL,
+    token: element._token,
+  });
+
+  console.log(feedData);
+};
+
+export const CreateShadowDom = ({ element }: { element: UMDNewsFeedType }) => {
+  const feedData = CreateFeed({ element });
   const container = document.createElement('div');
   const linkWrapper = CreateCallToAction();
 

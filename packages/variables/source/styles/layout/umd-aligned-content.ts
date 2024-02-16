5b32f1d28f98e8f0948f0fb8135a38632d1bfee6
Alia Pirruccello <98843063+aliapirruccello@users.noreply.github.com>
2024-02-16 09:08:11 -0500

    Close DSYS-565: Create reusable styles for Section Intro (#47)
    Rename from umd-links-group to umd-flex-row-auto
    
    Co-authored-by: Alia <apccello@umd.edu>

---
+++ packages/variables/source/styles/layout/umd-aligned-content.ts
@@ -0,0 +1,47 @@
+import { queries } from '../../tokens/breakpoints';
+import { spacing } from '../../tokens/spacing';
+
+const umdAlignedContentBase = {
+  display: 'block',
+
+  '& > *': {
+    textWrap: 'pretty',
+    marginTop: spacing.sm,
+
+    [`@media (${queries.large.min})`]: {
+      marginTop: spacing.md,
+    },
+
+    '&:first-child': {
+      marginTop: '0',
+    },
+  },
+};
+
+const umdAlignedContent = {
+  '.umd-aligned-content': { ...umdAlignedContentBase },
+
+  '.umd-layout-aligned-right': {
+    ...umdAlignedContentBase,
+    ...{
+      textAlign: 'right',
+
+      '& > *': {
+        justifyContent: 'flex-end',
+      },
+    },
+  },
+
+  '.umd-layout-aligned-center': {
+    ...umdAlignedContentBase,
+    ...{
+      textAlign: 'center',
+
+      '& > *': {
+        justifyContent: 'center',
+      },
+    },
+  },
+};
+
+export { umdAlignedContent };

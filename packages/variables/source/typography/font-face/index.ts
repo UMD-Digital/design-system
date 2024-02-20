f157057643a3b92c293ee6fc2d92c6ee01809895
Max Escher <mescher@umd.edu>
2024-02-20 10:57:29 -0500

    Close DSYS-585: Cut font-face markup to future proofing

--- packages/variables/source/typography/font-face/index.ts
+++ packages/variables/source/typography/font-face/index.ts
@@ -2,31 +2,17 @@ import barlowCondensed from './barlow-condensed';
 import crimsonPro from './crimson-pro';
 import interstate from './interstate';
 
-const { BoldItalic: BarlowCondensedBoldItalic } = barlowCondensed;
-const { Light: CrimsonProLight, Regular: CrimsonProRegular } = crimsonPro;
-const {
-  Thin: InterstateThin,
-  ExtraLight: InterstateExtraLight,
-  Light: InterstateLight,
-  Regular: InterstateRegular,
-  Italic: InterstateItalic,
-  Bold: InterstateBold,
-  BoldItalic: InterstateBoldItalic,
-  Black: InterstateBlack,
-  UltraBlack: InterstateUltraBlack,
-} = interstate;
-
 export const FontFaces = {
-  BarlowCondensedBoldItalic,
-  CrimsonProLight,
-  CrimsonProRegular,
-  InterstateThin,
-  InterstateExtraLight,
-  InterstateLight,
-  InterstateRegular,
-  InterstateItalic,
-  InterstateBold,
-  InterstateBoldItalic,
-  InterstateBlack,
-  InterstateUltraBlack,
+  BarlowCondensedBoldItalic: barlowCondensed.BoldItalic,
+  CrimsonProLight: crimsonPro.Light,
+  CrimsonProRegular: crimsonPro.Regular,
+  InterstateThin: interstate.Thin,
+  InterstateExtraLight: interstate.ExtraLight,
+  InterstateLight: interstate.Light,
+  InterstateRegular: interstate.Regular,
+  InterstateItalic: interstate.Italic,
+  InterstateBold: interstate.Bold,
+  InterstateBoldItalic: interstate.BoldItalic,
+  InterstateBlack: interstate.Black,
+  InterstateUltraBlack: interstate.UltraBlack,
 };

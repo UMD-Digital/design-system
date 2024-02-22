b85ba0e1812c9d0d89250c2f962f5773c53080a0
Alia Pirruccello <98843063+aliapirruccello@users.noreply.github.com>
2024-02-22 10:50:20 -0500

    Close MEV-124: Extract Watermark classes to Theme (#55)
    Co-authored-by: Alia <apccello@umd.edu>

--- packages/theme/source/common/index.ts
+++ packages/theme/source/common/index.ts
@@ -2,10 +2,12 @@ import FontFace from './font-faces';
 import Fonts from './typography';
 import RichText from './text/rich-text';
 import Ribbon from './text/ribbon';
+import Watermark from './text/watermark';
 
 export default {
   ...FontFace,
   ...Fonts,
   ...RichText,
   ...Ribbon,
+  ...Watermark,
 };
---
+++ packages/theme/source/common/text/watermark.ts
@@ -0,0 +1,53 @@
+import { Tokens } from '@universityofmaryland/variables';
+
+const { Colors, Queries, FontSize } = Tokens;
+
+const WatermarkContainer = {
+  position: 'relative',
+};
+
+const Watermark = {
+  alignItems: 'center',
+  color: Colors.gray.lighter,
+  position: 'absolute',
+  width: '100%',
+  top: '30px',
+  left: '-40px',
+  fontWeight: 'bold',
+  display: 'flex',
+  justifyContent: 'flex-start',
+  opacity: ' 0.7',
+  pointerEvents: 'none',
+  userSelect: 'none',
+  zIndex: '-1',
+
+  [`@media (${Queries.large.max})`]: {
+    display: 'none',
+  },
+
+  [`@media (${Queries.large.min})`]: {
+    fontSize: `min(calc(${FontSize['5xl']} + 13vw), 240px)`,
+  },
+};
+
+const WatermarkDark = {
+  ...Watermark,
+  ...{
+    opacity: '0.2',
+    zIndex: '0',
+  },
+};
+
+export default {
+  '.umd-watermark-container': {
+    ...WatermarkContainer,
+  },
+
+  '.umd-watermark': {
+    ...Watermark,
+  },
+
+  '.umd-watermark-dark': {
+    ...WatermarkDark,
+  },
+};

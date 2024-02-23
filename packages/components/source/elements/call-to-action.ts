94216749326a6b670ee0a6355bd66896d5839ed0
Josh Magness <magnessjo@gmail.com>
2024-02-23 14:07:48 -0500

    Component - Call To Action; SVG styling
    Ref DSYS-606

--- packages/components/source/elements/call-to-action.ts
+++ packages/components/source/elements/call-to-action.ts
@@ -119,7 +119,16 @@ const GetIcon = ({ element }: { element: HTMLElement }) => {
   const svgIcon = element.querySelector(`svg`);
   const imgIcon = element.querySelector(`img`);
 
-  if (svgIcon) return svgIcon;
+  if (svgIcon) {
+    const colorSet = svgIcon.getAttribute('color');
+
+    if (colorSet) {
+      const path = svgIcon.querySelector('path');
+      if (path) path.style.fill = colorSet;
+    }
+
+    return svgIcon;
+  }
   if (imgIcon) return imgIcon;
   return null;
 };

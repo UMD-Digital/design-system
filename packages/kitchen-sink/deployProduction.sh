4c5ef540fdd1efff234d8d6c951895e64e8d5bcf
Josh Magness <magnessjo@gmail.com>
2024-03-04 16:31:05 -0500

    Kitchen sink deployment scripts

--- packages/kitchen-sink/deployProduction.sh
+++ packages/kitchen-sink/deployProduction.sh
@@ -1,1 +1,1 @@
-rsync -azP --delete ./build/* ec2-user@3.20.248.56:/srv/http/kitchen-sink/
+rm -rf build && npx yarn build && rsync -azhn --delete ./build/* ec2-user@3.20.248.56:/srv/http/kitchen-sink/ 
--- packages/kitchen-sink/deployStaging.sh
+++ packages/kitchen-sink/deployStaging.sh
@@ -1,1 +1,1 @@
-rsync -azP --delete ./build/* staging:/srv/http/kitchen-sink/
+rm -rf build && npx yarn build && rsync -azhn --delete ./build/* ec2-user@3.131.127.17:/srv/http/kitchen-sink/ 

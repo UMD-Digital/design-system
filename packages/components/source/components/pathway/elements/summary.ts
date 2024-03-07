ef426b5e3e0b60a32d23de2f192f2c4bacfd0dd0
Josh Magness <magnessjo@gmail.com>
2024-03-07 14:33:14 -0500

    Component - Pathway; font styling

--- packages/components/source/components/pathway/elements/summary.ts
+++ packages/components/source/components/pathway/elements/summary.ts
@@ -118,6 +118,10 @@ const WithHeroStyles = `
       [`.${PATHWAY_CONTAINER}${WITH_HERO} .${SUMMARY_TEXT_CONTAINER} *`]: SansLarger,
     },
   })}
+
+  .${PATHWAY_CONTAINER}${WITH_HERO} .${SUMMARY_TEXT_CONTAINER} * {
+    font-weight: 700;
+  }
 `;
 
 // prettier-ignore

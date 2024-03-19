82dcb96ec416e5f0c54adfb0c9a4c3e34550ee0f
Josh Magness <magnessjo@gmail.com>
2024-03-19 16:51:44 -0400

    Theme - Fix(card icons) - missing flex styling
    Ref DSYS-671

--- packages/theme/source/animations/cards-icon.ts
+++ packages/theme/source/animations/cards-icon.ts
@@ -1,7 +1,93 @@
+import { Layout, Tokens } from '@universityofmaryland/variables';
 import { FlexColumnsCore } from '../layout/flex';
-import { Layout } from '@universityofmaryland/variables';
 
-const { FlexChildrenThree, FlexChildrenFour } = Layout;
+// Deprecated - Do Not Use
+
+const roundToThree = (number: number): number =>
+  Math.round(number * 1000) / 1000;
+
+const { Queries, Spacing } = Tokens;
+
+const FlexChildrenTwo = {
+  width: '100%',
+
+  [`@media (${Queries.large.min})`]: {
+    width: `calc(50% - ${roundToThree(parseInt(Spacing.lg) / 2)}px)`,
+  },
+
+  [`@media (${Queries.desktop.min})`]: {
+    width: `calc(50% - ${roundToThree(parseInt(Spacing['4xl']) / 2)}px)`,
+  },
+};
+
+const FlexChildrenThree = {
+  width: '100%',
+
+  [`@media (${Queries.large.min})`]: {
+    width: `calc(50% - ${roundToThree(parseInt(Spacing['lg']) / 2)}px)`,
+  },
+
+  [`@media (${Queries.desktop.min})`]: {
+    width: `calc(33.333% - ${roundToThree(
+      (parseInt(Spacing['lg']) * 2) / 3,
+    )}px)`,
+  },
+};
+
+const FlexChildrenFour = {
+  width: '100%',
+
+  [`@media (${Queries.large.min})`]: {
+    width: `calc(50% - ${roundToThree(parseInt(Spacing['lg']) / 2)}px)`,
+  },
+
+  [`@media (${Queries.desktop.min})`]: {
+    width: `calc(25% - ${roundToThree((parseInt(Spacing['lg']) * 3) / 4)}px)`,
+  },
+};
+
+const FlexColumnsContent = {
+  '.umd-flex > *': {
+    ...FlexChildrenTwo,
+    width: '100%',
+
+    [`@media (${Queries.large.min})`]: {
+      width: `calc(50% - ${roundToThree(parseInt(Spacing.lg) / 2)}px)`,
+    },
+
+    [`@media (${Queries.desktop.min})`]: {
+      width: `calc(50% - ${roundToThree(parseInt(Spacing['4xl']) / 2)}px)`,
+    },
+  },
+
+  '.umd-flex-three > *': {
+    ...FlexChildrenThree,
+    width: '100%',
+
+    [`@media (${Queries.large.min})`]: {
+      width: `calc(50% - ${roundToThree(parseInt(Spacing['lg']) / 2)}px)`,
+    },
+
+    [`@media (${Queries.desktop.min})`]: {
+      width: `calc(33.333% - ${roundToThree(
+        (parseInt(Spacing['lg']) * 2) / 3,
+      )}px)`,
+    },
+  },
+
+  '.umd-flex-four > *': {
+    ...FlexChildrenFour,
+    width: '100%',
+
+    [`@media (${Queries.large.min})`]: {
+      width: `calc(50% - ${roundToThree(parseInt(Spacing['lg']) / 2)}px)`,
+    },
+
+    [`@media (${Queries.desktop.min})`]: {
+      width: `calc(25% - ${roundToThree((parseInt(Spacing['lg']) * 3) / 4)}px)`,
+    },
+  },
+};
 
 export const FloatCardsIcon = {
   '.umd-flex-three-float-icon-card': {

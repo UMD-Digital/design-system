ef66634d1037a7d5914739ea6ca082c3435391f5
Josh Magness <magnessjo@gmail.com>
2024-04-06 15:11:03 -0400

    Component - Remove example directory

--- packages/components/source/components/_example/globals.ts
+++
@@ -1,7 +0,0 @@
-export const BREAKPOINTS = {};
-export const ELEMENTS = {};
-export const SLOTS = {};
-export const VARIABLES = {
-  ELEMENT_NAME: 'umd-element-example',
-};
-export const REFERENCES = {};

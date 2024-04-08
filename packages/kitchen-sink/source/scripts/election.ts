e07b01fc2553c33734daad9a8fe0b618e47dfbf2
Alia <apccello@umd.edu>
2024-04-08 17:55:16 -0400

    MEV-159: Election Template

---
+++ packages/kitchen-sink/source/scripts/election.ts
@@ -0,0 +1,8 @@
+require('styles/election.css');
+
+import '@universityofmaryland/utilityheader';
+import LoadUmdComponents from '@universityofmaryland/web-components-library';
+
+document.addEventListener('DOMContentLoaded', () => {
+  LoadUmdComponents();
+});

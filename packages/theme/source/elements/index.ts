a346a32d0fe7147bb5327f3b1b76662827df1c9c
Josh Magness <magnessjo@gmail.com>
2024-04-14 14:46:58 -0400

    Theme - Remove unused styles
    Ref DSYS-971

--- packages/theme/source/elements/index.ts
+++ packages/theme/source/elements/index.ts
@@ -1,17 +1,7 @@
 import { Loader } from './loader';
 import { CaptionedMedia } from './media';
-import Listing from './listing';
-import PeopleListing from './people-listing';
-import EventListing from './event-listing';
-import EventFlags from './event-flags';
-import FeaturedEvent from './featured-event';
 
 export default {
   ...Loader,
   ...CaptionedMedia,
-  ...Listing,
-  ...PeopleListing,
-  ...EventListing,
-  ...EventFlags,
-  ...FeaturedEvent,
 };

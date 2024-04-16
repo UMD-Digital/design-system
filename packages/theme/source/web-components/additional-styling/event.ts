46ee9f576589a1a6da97bcb4339e27dc1957e4e0
Josh Magness <magnessjo@gmail.com>
2024-04-16 11:40:18 -0400

    Theme - Featured Event style on mobile
    Ref DSYS-987

--- packages/theme/source/web-components/additional-styling/event.ts
+++ packages/theme/source/web-components/additional-styling/event.ts
@@ -1,5 +1,7 @@
 import { Tokens } from '@universityofmaryland/variables';
 
+const { Queries, Colors, Spacing } = Tokens;
+
 export default {
   'umd-element-event:not(:defined)': {
     display: 'none',
@@ -9,5 +11,11 @@ export default {
   },
   'umd-element-event[display="list"] + umd-element-event[display="list"]': {
     marginTop: Tokens.Spacing.md,
+  },
+  'umd-element-event[display="feature"]': {
+    [`@media (${Queries.large.max})`]: {
+      borderBottom: `1px solid ${Colors.gray.light}`,
+      paddingBottom: `${Spacing.md}`,
+    },
   },
 };

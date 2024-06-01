5eed7fc0bd40074da6e4d09be7d033a2a7904a70
Josh Magness <magnessjo@gmail.com>
2024-06-01 13:16:05 -0400

    Theme - Carousel Cards style

---
+++ packages/theme/source/web-components/additional-styling/carousel-cards.ts
@@ -0,0 +1,10 @@
+import { Tokens } from '@universityofmaryland/variables';
+
+export default {
+  'umd-element-carousel-cards:not(:defined)': {
+    display: 'none',
+  },
+  'umd-element-carousel-cards:defined': {
+    display: 'block',
+  },
+};

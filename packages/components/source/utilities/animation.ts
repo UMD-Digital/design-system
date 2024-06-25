7f6025bd76090d6c5ef61ad9cf3be222c60487d4
Josh Magness <magnessjo@gmail.com>
2024-06-25 12:06:50 -0400

    Macro - Scroll To Animation
    Ref DSYS-1411

--- packages/components/source/utilities/animation.ts
+++ packages/components/source/utilities/animation.ts
@@ -36,6 +36,53 @@ const ShrinkThenRemove = ({ container }: { container: HTMLElement }) => {
   window.requestAnimationFrame(shrink);
 };
 
+const ScrollTo = ({
+  element,
+  spread = 30,
+  frames = 60,
+}: {
+  element: HTMLElement;
+  spread?: number;
+  frames?: number;
+}) => {
+  if (!element) return;
+
+  const documentBody =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  const elm = document.body;
+  let from = 0;
+  const to = element.getBoundingClientRect();
+  const toPosition =
+    documentBody == 0 ? to.top - from - spread : to.top - spread;
+  const currentPosition = window.pageYOffset;
+  const jump = (toPosition - from) / frames;
+  from = currentPosition;
+
+  function scroll() {
+    if (frames > 0) {
+      const position = from + jump;
+
+      from = position;
+      elm.scrollTop = from;
+      document.documentElement.scrollTop = from;
+
+      frames--;
+      window.requestAnimationFrame(scroll);
+    } else {
+      const elms = Array.from(
+        element.querySelectorAll('a, button'),
+      ) as Array<HTMLElement>;
+      element.focus({ preventScroll: true });
+      if (Array.isArray(elms) && elms.length > 0) {
+        elms[0].focus({ preventScroll: true });
+      }
+    }
+  }
+
+  window.requestAnimationFrame(scroll);
+};
+
 export default {
   ShrinkThenRemove,
+  ScrollTo,
 };

bde2d06da4a2c22416d93281d8e406ee6aa85c57
Josh Magness <magnessjo@gmail.com>
2024-07-18 10:45:51 -0400

    Utility - events (hotfix)
    Ref DSYS-1531

--- packages/components/source/utilities/date.ts
+++ packages/components/source/utilities/date.ts
@@ -1,32 +1,26 @@
 const CreateVisualFormattedDate = (date: Date) => {
   return {
     full: date.toLocaleString('en-US', {
-      timeZone: 'UTC',
       month: 'short',
       year: 'numeric',
       day: 'numeric',
     }),
     dayOfWeekLong: date.toLocaleString('en-US', {
-      timeZone: 'UTC',
       weekday: 'long',
     }),
     dayOfWeek: date.toLocaleString('en-US', {
-      timeZone: 'UTC',
       weekday: 'short',
     }),
     month: date.toLocaleString('en-US', {
-      timeZone: 'UTC',
       month: 'short',
     }),
     day: date.toLocaleString('en-US', {
-      timeZone: 'UTC',
       day: 'numeric',
     }),
     time: date
       .toLocaleTimeString('en-US', {
         hour: 'numeric',
         minute: 'numeric',
-        timeZone: 'America/New_York',
       })
       .replace(' AM', 'am')
       .replace(' PM', 'pm'),

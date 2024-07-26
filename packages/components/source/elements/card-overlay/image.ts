7caf78291b0abfb2154cb99dc3cb4498b278bd2a
Josh Magness <magnessjo@gmail.com>
2024-07-26 14:31:13 -0400

    Element - Overlay Card; quote option
    Ref DSYS-1485

--- packages/components/source/elements/card-overlay/image.ts
+++ packages/components/source/elements/card-overlay/image.ts
@@ -1,6 +1,7 @@
 import { Tokens } from '@universityofmaryland/variables';
 import { LayoutBlockOverlay, TextLockupSmall } from 'macros';
 import CtaIcon, { TypeCardOverlayCtaIcon } from './elements/icon-cta';
+import { AssetIcon, MarkupCreate } from 'utilities';
 
 type TypeCardOverlayImageElement = TypeCardOverlayCtaIcon & {
   image: HTMLImageElement | null;
@@ -8,14 +9,17 @@ type TypeCardOverlayImageElement = TypeCardOverlayCtaIcon & {
   text: HTMLElement | null;
   eyebrow?: HTMLElement | null;
   actions?: HTMLElement | null;
+  isQuote?: boolean;
 };
 
 const { Spacing, Colors } = Tokens;
+const { QUOTE } = AssetIcon;
 
 const ELEMENT_NAME = 'umd-card-overlay-image';
 const ATTRIBUTE_CTA_ICON = 'cta-icon';
 
 const ELEMENT_CARD_OVERLAY_IMAGE_DECLARATION = 'card-overlay-image-declaration';
+const ELEMENT_CARD_OVERLAY_QUOTE = 'card-overlay-image-quote';
 
 const IS_WITH_CTA_ICON = `[${ATTRIBUTE_CTA_ICON}]`;
 
@@ -37,6 +41,18 @@ const OverwriteCtaIcon = `
   }
 `;
 
+const QuoteStyles = `
+  .${ELEMENT_CARD_OVERLAY_QUOTE} {
+    width: 41px;
+    height: 30px;
+    margin-bottom: ${Spacing.xs};
+  }
+
+  .${ELEMENT_CARD_OVERLAY_QUOTE} svg {
+    fill: ${Colors.red};
+  }
+`;
+
 // prettier-ignore
 const STYLES_OVERLAY_CARD_ELEMENT = `
   .${ELEMENT_CARD_OVERLAY_IMAGE_DECLARATION} {
@@ -50,9 +66,11 @@ const STYLES_OVERLAY_CARD_ELEMENT = `
 
   ${LayoutBlockOverlay.Styles}
   ${OverwriteCtaIcon}
+  ${QuoteStyles}
 `;
 
 const CreateCardOverlayElement = (props: TypeCardOverlayImageElement) => {
+  const { isQuote } = props;
   const elementDeclaration = document.createElement('div');
   const blockOverlayContainer = LayoutBlockOverlay.CreateElement({
     ...props,
@@ -61,6 +79,20 @@ const CreateCardOverlayElement = (props: TypeCardOverlayImageElement) => {
   const ctaIcon = CtaIcon.CreateElement({ ...props, theme: 'dark' });
 
   if (blockOverlayContainer) {
+    if (isQuote) {
+      const lockup = blockOverlayContainer.querySelector(
+        `.${TextLockupSmall.Elements.container}`,
+      );
+
+      if (lockup) {
+        const quoteWrapper = document.createElement('div');
+        quoteWrapper.classList.add(ELEMENT_CARD_OVERLAY_QUOTE);
+        quoteWrapper.innerHTML = QUOTE;
+
+        lockup.insertBefore(quoteWrapper, lockup.firstChild);
+      }
+    }
+
     if (ctaIcon) {
       elementDeclaration.setAttribute(ATTRIBUTE_CTA_ICON, '');
       blockOverlayContainer.appendChild(ctaIcon);

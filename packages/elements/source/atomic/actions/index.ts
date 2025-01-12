caadedaf2874539fa00e2d95c71fb6b6f8e82ad1
Josh Magness <magnessjo@gmail.com>
2025-01-12 11:20:52 -0500

    Action element - type setting and reusable functions
    Ref DSYS-1905

--- packages/elements/source/atomic/actions/index.ts
+++ packages/elements/source/atomic/actions/index.ts
@@ -1,7 +1,7 @@
 import * as Utility from 'utilities';
 import { ElementModel } from 'model';
 
-interface ElmentProps {
+interface ElementProps {
   element: HTMLElement;
   isThemeDark?: boolean;
   isThemeGold?: boolean;
@@ -9,245 +9,134 @@ interface ElmentProps {
   elementStyles?: Record<string, any>;
 }
 
-interface OptionProps extends ElmentProps {
-  isTypePrimary?: boolean;
-  isTypeSecondary?: boolean;
-  isTypeOutline?: boolean;
-  plainText?: HTMLElement | null;
+type ElementType = 'primary' | 'secondary' | 'outline';
+type IconType = 'email' | 'newWindow' | 'document' | 'fearless';
+
+type ActionFunction = (props: ElementProps) => {
+  element: HTMLElement;
+  styles: string;
+};
+
+type ActionVariants = {
+  default: ActionFunction;
+  large?: ActionFunction;
+  dark?: ActionFunction;
+  gold?: ActionFunction;
+};
+
+type Actions = Record<ElementType, ActionVariants>;
+
+const ICONS: Record<IconType, string> = {
+  email: Utility.asset.icon.EMAIL,
+  newWindow: Utility.asset.icon.NEW_WINDOW,
+  document: Utility.asset.icon.DOCUMENT,
+  fearless: Utility.asset.icon.FEARLESS,
+};
+
+function insertIcon(element: HTMLElement, svg: string): void {
+  const icon = Utility.markup.create.svgFromString(svg);
+  if (icon) element.insertBefore(icon, element.firstChild);
 }
 
-const TYPE_PRIMARY = 'primary';
-const TYPE_SECONDARY = 'secondary';
-const TYPE_OUTLINE = 'outline';
+function createLinkIcon(element: HTMLElement, type: ElementType): void {
+  const existingIcon = Utility.markup.get.icon({ element });
+  if (existingIcon) {
+    element.insertBefore(existingIcon, element.firstChild);
+    return;
+  }
 
-const CreateLinkIcon = ({
-  element,
-  type,
-  isLink,
-}: {
-  element: HTMLElement;
-  type: string;
-  isLink: boolean;
-}) => {
-  const icon = Utility.markup.get.icon({ element });
+  const href = element.getAttribute('href');
   const isExternalTab = element.getAttribute('target') === '_blank';
   const isDownload = element.getAttribute('download') !== null;
-  const isMail = element.getAttribute('href')?.includes('mailto:');
+  const isMail = href?.includes('mailto:');
 
-  if (icon) {
-    element.insertBefore(icon, element.firstChild);
-    return;
+  if (isMail) {
+    insertIcon(element, ICONS.email);
+  } else if (isExternalTab) {
+    insertIcon(element, ICONS.newWindow);
+  } else if (isDownload) {
+    insertIcon(element, ICONS.document);
+  } else if (type === 'secondary') {
+    insertIcon(element, ICONS.fearless);
   }
+}
 
-  if (isMail && isLink) {
-    // To Do - add space for small icon
-    const mailIcon = Utility.markup.create.svgFromString(
-      Utility.asset.icon.EMAIL,
-    );
-    if (mailIcon) element.insertBefore(mailIcon, element.firstChild);
-    return;
-  }
-
-  if (isExternalTab && isLink) {
-    const windowIcon = Utility.markup.create.svgFromString(
-      Utility.asset.icon.NEW_WINDOW,
-    );
-    if (windowIcon) element.insertBefore(windowIcon, element.firstChild);
-    return;
-  }
-
-  if (isDownload && isLink) {
-    const documentIcon = Utility.markup.create.svgFromString(
-      Utility.asset.icon.DOCUMENT,
-    );
-    if (documentIcon) element.insertBefore(documentIcon, element.firstChild);
-    return;
-  }
-
-  if (type === TYPE_SECONDARY) {
-    const fearlessIcon = Utility.markup.create.svgFromString(
-      Utility.asset.icon.FEARLESS,
-    );
-    if (fearlessIcon) element.insertBefore(fearlessIcon, element.firstChild);
-    return;
-  }
-};
-
-export const primary = ({
-  element,
-  isSizeLarge,
-  isThemeGold,
-  elementStyles,
-}: ElmentProps) => {
-  CreateLinkIcon({ element, type: TYPE_PRIMARY, isLink: true });
-
-  if (isThemeGold) {
-    const white = ElementModel.actions.primaryWhite({
-      element,
-      elementStyles,
-    });
-
-    return {
-      element: white.element,
-      styles: white.styles,
-    };
-  }
-
-  if (isSizeLarge) {
-    const large = ElementModel.actions.primaryLarge({
-      element,
-      elementStyles,
-    });
-
-    return {
-      element: large.element,
-      styles: large.styles,
-    };
-  }
-
-  const normal = ElementModel.actions.primary({
-    element,
-    elementStyles,
+function createElementWithStyle(
+  actionFn: (props: ElementProps) => { element: HTMLElement; styles: string },
+  props: ElementProps,
+) {
+  const result = actionFn({
+    element: props.element,
+    elementStyles: props.elementStyles,
   });
+  return { element: result.element, styles: result.styles };
+}
 
-  return {
-    element: normal.element,
-    styles: normal.styles,
+function createElement(type: ElementType, props: ElementProps) {
+  createLinkIcon(props.element, type);
+
+  const actions: Actions = {
+    primary: {
+      default: ElementModel.actions.primary,
+      large: ElementModel.actions.primaryLarge,
+      gold: ElementModel.actions.primaryWhite,
+    },
+    secondary: {
+      default: ElementModel.actions.secondary,
+      large: ElementModel.actions.secondaryLarge,
+      dark: ElementModel.actions.secondaryWhite,
+      gold: ElementModel.actions.secondaryGold,
+    },
+    outline: {
+      default: ElementModel.actions.outline,
+      large: ElementModel.actions.outlineLarge,
+      dark: ElementModel.actions.outlineWhite,
+    },
   };
-};
 
-export const secondary = ({
-  element,
-  isSizeLarge,
-  isThemeDark,
-  isThemeGold,
-  elementStyles,
-}: ElmentProps) => {
-  CreateLinkIcon({ element, type: TYPE_SECONDARY, isLink: true });
+  const typeActions = actions[type];
 
-  if (isThemeDark) {
-    const white = ElementModel.actions.secondaryWhite({
-      element,
-      elementStyles,
-    });
-
-    return {
-      element: white.element,
-      styles: white.styles,
-    };
+  if (props.isThemeGold && typeActions.gold) {
+    return createElementWithStyle(typeActions.gold, props);
   }
-
-  if (isThemeGold) {
-    const white = ElementModel.actions.secondaryGold({
-      element,
-      elementStyles,
-    });
-
-    return {
-      element: white.element,
-      styles: white.styles,
-    };
+  if (props.isThemeDark && typeActions.dark) {
+    return createElementWithStyle(typeActions.dark, props);
   }
-
-  if (isSizeLarge) {
-    const large = ElementModel.actions.secondaryLarge({
-      element,
-      elementStyles,
-    });
-
-    return {
-      element: large.element,
-      styles: large.styles,
-    };
+  if (props.isSizeLarge && typeActions.large) {
+    return createElementWithStyle(typeActions.large, props);
   }
+  return createElementWithStyle(typeActions.default, props);
+}
 
-  const normal = ElementModel.actions.secondary({
-    element,
-    elementStyles,
-  });
+export const primary = (props: ElementProps) => createElement('primary', props);
+export const secondary = (props: ElementProps) =>
+  createElement('secondary', props);
+export const outline = (props: ElementProps) => createElement('outline', props);
 
-  return {
-    element: normal.element,
-    styles: normal.styles,
-  };
-};
-
-export const outline = ({
-  element,
-  isSizeLarge,
-  isThemeDark,
-  elementStyles,
-}: ElmentProps) => {
-  CreateLinkIcon({ element, type: TYPE_OUTLINE, isLink: true });
-
-  if (isThemeDark) {
-    const white = ElementModel.actions.outlineWhite({
-      element,
-      elementStyles,
-    });
-
-    return {
-      element: white.element,
-      styles: white.styles,
-    };
-  }
-
-  if (isSizeLarge) {
-    const large = ElementModel.actions.outlineLarge({
-      element,
-      elementStyles,
-    });
-
-    return {
-      element: large.element,
-      styles: large.styles,
-    };
-  }
-
-  const normal = ElementModel.actions.outline({
-    element,
-    elementStyles,
-  });
-
-  return {
-    element: normal.element,
-    styles: normal.styles,
-  };
-};
-
-export const options = (props: OptionProps) => {
-  const { isTypePrimary, isTypeSecondary, isTypeOutline, plainText } = props;
+export function options(
+  props: ElementProps & {
+    isTypePrimary?: boolean;
+    isTypeSecondary?: boolean;
+    isTypeOutline?: boolean;
+    plainText?: HTMLElement | null;
+  },
+) {
   const container = document.createElement('div');
-  // const hasPlainText = plainTextElement.length > 0;
   let styles = '';
 
-  if (isTypePrimary) {
-    const primaryElement = primary(props);
-    container.appendChild(primaryElement.element);
-    styles = primaryElement.styles;
+  if (props.isTypePrimary) {
+    const result = primary(props);
+    container.appendChild(result.element);
+    styles = result.styles;
+  } else if (props.isTypeSecondary) {
+    const result = secondary(props);
+    container.appendChild(result.element);
+    styles = result.styles;
+  } else if (props.isTypeOutline) {
+    const result = outline(props);
+    container.appendChild(result.element);
+    styles = result.styles;
   }
 
-  if (isTypeSecondary) {
-    const secondaryElement = secondary(props);
-    container.appendChild(secondaryElement.element);
-    styles = secondaryElement.styles;
-  }
-
-  if (isTypeOutline) {
-    const outlineElement = outline(props);
-    container.appendChild(outlineElement.element);
-    styles = outlineElement.styles;
-  }
-
-  // if (hasPlainText) {
-  //   const plainTextSlot = Node.slot({ type: PLAIN_TEXT });
-  //   plainTextSlot.classList.add(CTA_PLAIN_TEXT_SLOT);
-  //   container.setAttribute(ATTRIBUTE_PLAIN_TEXT, 'true');
-  //   container.appendChild(plainTextSlot);
-  // }
-
-  return {
-    element: container,
-    styles,
-  };
-};
+  return { element: container, styles };
+}

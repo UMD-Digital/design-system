261ce586b8191cf4fadbcbdecb3d0de17e99ddbe
Josh Magness <magnessjo@gmail.com>
2025-02-04 13:13:01 -0500

    Actions - add dark option
    Ref DSYS-2020

--- packages/elements/source/atomic/actions/index.ts
+++ packages/elements/source/atomic/actions/index.ts
@@ -87,6 +87,7 @@ function createElement(type: ElementType, props: ElementProps) {
     primary: {
       default: ElementModel.actions.primary,
       large: ElementModel.actions.primaryLarge,
+      dark: ElementModel.actions.primaryWhite,
     },
     secondary: {
       default: ElementModel.actions.secondary,

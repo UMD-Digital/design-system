b32682b923e54313260d8b89c53386eb1f4539ca
Josh Magness <magnessjo@gmail.com>
2025-02-11 14:43:12 -0500

    Skip content styles
    Ref DSYS-2033

--- packages/styles/source/accessibility/skip.ts
+++ packages/styles/source/accessibility/skip.ts
@@ -3,13 +3,25 @@ import { create } from '../utilities';
 // umd-skip-content
 export const content = create.jssObject({
   className: 'umd-skip-content',
-  clip: 'rect(0,0,0,0)',
-  borderWidth: '0px',
-  height: '1px',
-  margin: '-1px',
+  backgroundColor: `#fff`,
+  color: `#e21833`,
+  display: `block`,
+  height: `0`,
+  width: '0',
+  opacity: `0`,
+  padding: `8px`,
+  position: `absolute`,
+  textDecoration: `underline`,
+  transition: `none`,
   overflow: 'hidden',
-  padding: '0',
-  position: 'absolute',
   whiteSpace: 'nowrap',
-  width: '1px',
+
+  '&:focus': {
+    display: 'block',
+    height: 'inherit',
+    width: 'auto',
+    zIndex: '9999',
+    opacity: '1',
+    transition: 'opacity 0.5s',
+  },
 });

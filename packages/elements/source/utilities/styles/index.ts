88a860f9264f5ab1bac7fcdf9e69002e15c3e1eb
Josh Magness <magnessjo@gmail.com>
2025-02-23 12:48:04 -0500

    Parsed element specific nested JSS
    This function should be removed in the near future. It’s purpose is to be a bridge until we refactor feeds to use the style combination method.
    
    Ref DSYS-2125

--- packages/elements/source/utilities/styles/index.ts
+++ packages/elements/source/utilities/styles/index.ts
@@ -23,3 +23,53 @@ export const getStyleStringFromJssObject = (styleObj: Styles.JssEntry) =>
       [`.${styleObj.className}`]: styleObj,
     },
   });
+
+// To be deleted after refactor is done.
+
+export const parseNestedJSStoCSS = (
+  styles: Record<string, any>,
+  className: string,
+) => {
+  const toKebabCase = (str: string) => {
+    return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
+  };
+
+  const processStyles = (obj: Record<string, any>, selector: string) => {
+    let css = `${selector} {\n`;
+
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value === 'object') continue;
+
+      const property = toKebabCase(key);
+      css += `  ${property}: ${value};\n`;
+    }
+
+    css += '}\n';
+    return css;
+  };
+
+  let result = '';
+
+  for (const [key, value] of Object.entries(styles)) {
+    if (typeof value !== 'object') continue;
+
+    let selector;
+    switch (key) {
+      case 'element':
+        selector = `.${className}`;
+        break;
+      case 'siblingAfter':
+        selector = `* + .${className}`;
+        break;
+      case 'subElement':
+        selector = `.${className} + *`;
+        break;
+      default:
+        selector = `.${className}-${key}`;
+    }
+
+    result += processStyles(value, selector);
+  }
+
+  return result;
+};

cfcd1036a334b5456cc563fd1cffc040cb9b9010
Josh Magness <magnessjo@gmail.com>
2025-02-28 14:05:16 -0500

    Styles - Event meta

---
+++ packages/styles/source/element/event/meta.ts
@@ -0,0 +1,91 @@
+import { color, media, spacing } from '../../token';
+import { sans } from '../../typography';
+import { create } from '../../utilities';
+
+// Consistent naming
+const classNamePrefix = 'umd-element-event-meta';
+
+const mobileBreakpoint = 400;
+
+// umd-element-event-meta-container
+export const container = create.jssObject({
+  className: `${classNamePrefix}-container`,
+  container: 'inline-size',
+
+  '+ *': {
+    marginTop: `${spacing.sm}`,
+  },
+});
+
+// umd-element-event-meta-wrapper
+export const wrapper = create.jssObject({
+  className: `${classNamePrefix}-wrapper`,
+  display: 'flex',
+  flexWrap: 'wrap',
+
+  '> *': {
+    marginRight: `5px`,
+    marginTop: `5px`,
+  },
+
+  '> *:not(:first-child)': {
+    display: 'flex',
+    width: '100%',
+  },
+
+  '> *:first-child': {
+    display: 'flex',
+    width: '100%',
+
+    [`@container (min-width: ${mobileBreakpoint}px)`]: {
+      display: 'flex',
+    },
+
+    '> *': {
+      display: 'flex',
+      alignItems: 'center',
+    },
+
+    '> *:not(:first-child)': {
+      [`@container (min-width: ${mobileBreakpoint}px)`]: {
+        marginLeft: `${spacing.xs}`,
+      },
+
+      [`@container (max-width: ${mobileBreakpoint - 1}px)`]: {
+        marginTop: `3px`,
+      },
+    },
+  },
+});
+
+// umd-element-event-meta-date
+export const date = create.jssObject({
+  ...sans.smaller,
+  className: `${classNamePrefix}-date`,
+  color: `${color.gray.dark}`,
+  fontWeight: `400`,
+
+  '& > span': {
+    width: '18px',
+    display: 'flex',
+
+    [`@container (min-width: ${mobileBreakpoint}px)`]: {
+      width: '20px',
+    },
+  },
+
+  '& svg': {
+    width: '12px',
+    height: '12px',
+
+    [`@container (min-width: ${mobileBreakpoint}px)`]: {
+      width: '14px',
+      height: '14px',
+    },
+  },
+});
+
+// umd-element-event-meta-location
+export const location = create.jssObject({
+  className: `${classNamePrefix}-location`,
+});
--- packages/styles/source/element/index.ts
+++ packages/styles/source/element/index.ts
@@ -2,10 +2,22 @@ import * as action from './action';
 import * as asset from './asset';
 import * as composite from './composite';
 import * as embed from './embed';
+import * as event from './event';
 import * as field from './field';
 import * as form from './form';
 import * as list from './list';
 import * as table from './table';
 import * as text from './text';
 
-export { action, asset, composite, embed, field, form, list, text, table };
+export {
+  action,
+  asset,
+  composite,
+  embed,
+  event,
+  field,
+  form,
+  list,
+  text,
+  table,
+};

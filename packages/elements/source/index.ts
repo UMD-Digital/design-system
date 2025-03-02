2ae798aa328ad1dd6105cbf1ed13d0397144528b
Josh Magness <magnessjo@gmail.com>
2025-03-02 11:45:52 -0500

    Export model as part of elements package

--- packages/elements/source/index.ts
+++ packages/elements/source/index.ts
@@ -2,4 +2,5 @@ export * as Atomic from './atomic';
 export * as Composite from './composite';
 export * as Layout from './layout';
 export * as Macros from './macros';
+export * as Model from './model';
 export * as Utilities from './utilities';

8e836ad193eb47775ae1343a2d396c7f319e19a1
Josh Magness <magnessjo@gmail.com>
2025-03-06 15:37:57 -0500

    Remove layout overlay
    Ref DSYS-2127

--- packages/elements/source/layout/index.ts
+++ packages/elements/source/layout/index.ts
@@ -1,9 +1,3 @@
-export {
-  default as blockOverlay,
-  elements as blockOverlayElements,
-  STYLES_BLOCK_OVERLAY_ELEMENT,
-} from './overlay';
-
 export { default as FixedFullScreen } from './carousel-full-screen';
 
 export { default as Image } from './image';

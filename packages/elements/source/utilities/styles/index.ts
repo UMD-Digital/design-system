ada8e9750449cdd11ec670ad618e1fb9bf98deeb
Josh Magness <magnessjo@gmail.com>
2025-03-08 13:27:35 -0500

    Element - Utility style: Remove duplicated styles
    Ref DSYS-2057

--- packages/elements/source/utilities/styles/index.ts
+++ packages/elements/source/utilities/styles/index.ts
@@ -1,5 +1,6 @@
 import * as Styles from '@universityofmaryland/web-styles-library';
 import postcss from 'postcss';
+import discardDuplicates from 'postcss-discard-duplicates';
 
 const postcssNesting = require('postcss-nesting');
 const postcssJs = require('postcss-js');
@@ -24,6 +25,13 @@ export const getStyleStringFromJssObject = (styleObj: Styles.JssEntry) =>
       [`.${styleObj.className}`]: styleObj,
     },
   });
+
+export const optimizedCss = async (css: string) => {
+  const result = await postcss([discardDuplicates()]).process(css, {
+    from: undefined,
+  });
+  return result.css;
+};
 
 // To be deleted after refactor is done.
 

f29970b6ea5c3aa47269a03fdbb52932986ed54b
Josh Magness <magnessjo@gmail.com>
2025-03-13 11:43:09 -0400

    Scaling fonts version 10
    Ref DSYS-2204

--- packages/styles/source/typography/sans.ts
+++ packages/styles/source/typography/sans.ts
@@ -6,7 +6,6 @@ const breakpointDesktop = media.queries.desktop.min;
 
 const scalingContainerSmall = media.breakpointValues.small.min;
 const scalingContainerMedium = media.breakpointValues.medium.min;
-const scalingContainerLarger = 600;
 
 const sizeExtraLarge = {
   fontSize: font.size['4xl'],
@@ -104,10 +103,6 @@ export const scalingLarger = {
   },
 
   [`@container (min-width: ${scalingContainerMedium}px)`]: {
-    ...sizeExtraLargeResponsive,
-  },
-
-  [`@container (min-width: ${scalingContainerLarger}px)`]: {
     fontSize: font.size['4xl'],
   },
 };

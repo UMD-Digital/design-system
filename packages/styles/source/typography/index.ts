77845c81fd8286caace8c33ef75656b9991c348a
Josh Magness <magnessjo@gmail.com>
2025-04-05 14:10:29 -0400

    Remove generated files for docus
    Fix dispatch syntax

--- packages/styles/source/typography/index.ts
+++ packages/styles/source/typography/index.ts
@@ -1,6 +1,70 @@
+/**
+ * @module typography
+ * Typography styles and class use cases
+ */
+
+/**
+ * @internal
+ */
+
 export * as fontFace from './font-face';
+
+/**
+ * Campaign fonts
+ * @example
+ * ```typescript
+ * import * as Styles from '@universityofmaryland/web-styles-library';
+ * Styles.campaign.fonts
+ * ```
+ * @since 1.8.0
+ */
+
 export * as campaign from './campaign';
+
+/**
+ * Elements fonts
+ * @example
+ * ```typescript
+ * import * as Styles from '@universityofmaryland/web-styles-library';
+ * Styles.elements.fonts
+ * ```
+ * @since 1.8.0
+ */
+
 export * as elements from './elements';
+
+/**
+ * Sans fonts
+ * @example
+ * ```typescript
+ * import * as Styles from '@universityofmaryland/web-styles-library';
+ * Styles.sans.fonts
+ * ```
+ * @since 1.8.0
+ */
+
 export * as sans from './sans';
+
+/**
+ * Serif fonts
+ * @example
+ * ```typescript
+ * import * as Styles from '@universityofmaryland/web-styles-library';
+ * Styles.serif.fonts
+ * ```
+ * @since 1.8.0
+ */
+
 export * as serif from './serif';
+
+/**
+ * Stat fonts
+ * @example
+ * ```typescript
+ * import * as Styles from '@universityofmaryland/web-styles-library';
+ * Styles.stats.fonts
+ * ```
+ * @since 1.8.0
+ */
+
 export * as stats from './stats';

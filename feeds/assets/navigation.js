6e3ed59d2f6ebf2c5c39c654b259acbdccba82f0
magnessjo <856595+magnessjo@users.noreply.github.com>
2025-04-11 15:32:27 +0000

    docs: update documentation [skip ci]

---
+++ feeds/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "eJydkN0KwjAMRt8l10Vh3vVVxi7qGl2wq9JmU5C9u+yfsa4Ub5N8hy8n/wLjh0GCKpXGmkoQ8FJcgYT6qRuD/jxvThXXBgQ8yGqQmYCyIqMdWpD5gvGGNLoV0ipH6rrBjCdb2iXrik4sFGzRst9XGedJRe6OdKjGhOjXuwprAUOeI+l+HUkfS5jyCQosvgMC+mnS+zdU3DgMKhgg80HkjSOFQ/5PgUM2qK/oih9/3t1y"

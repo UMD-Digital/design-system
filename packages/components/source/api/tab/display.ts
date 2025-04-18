0d5c6f1736da27fa4015e0769dc5c2366f432b9c
Josh Magness <magnessjo@gmail.com>
2025-04-18 12:43:15 -0400

    Component - Tab: Conversion to the element model

--- packages/components/source/api/tab/display.ts
+++ packages/components/source/api/tab/display.ts
@@ -1,33 +1,34 @@
-declare global {
-  interface Window {
-    UMDTabsElement: typeof UMDTabsElement;
-  }
-}
-
 import { Composite } from '@universityofmaryland/web-elements-library';
-import { Attributes, Slots } from 'model';
-import { Markup, Styles } from 'utilities';
+import { Attributes, Model, Register, Slots } from 'model';
+import { Markup } from 'utilities';
 
-const { Node } = Markup.create;
+const tagName = 'umd-element-tabs';
 
-const ELEMENT_NAME = 'umd-element-tabs';
+const slots = {
+  tabs: {
+    required: true,
+  },
+};
 
-const styles = `
-  :host {
-    display: block;
-  }
+const attributes = Attributes.handler.combine(
+  Attributes.handler.observe.resize({
+    callback: (element) => element.events?.resize(),
+  }),
+  Attributes.handler.observe.visuallyPosition({
+    callback: (element, topPosition) =>
+      element.events?.setTopPosition({ value: topPosition }),
+  }),
+  // Deprecated
+  Attributes.handler.observe.visuallyPosition({
+    name: Attributes.names.LAYOUT_STICKY_TOP,
+    callback: (element, topPosition) =>
+      element.events?.setTopPosition({ value: topPosition }),
+  }),
+);
 
-  ${Styles.reset}
-  ${Composite.tabs.standard.Styles}
-`;
-
-const styleTemplate = Markup.create.Node.stylesTemplate({ styles });
-
-const CreateShadowDom = ({ element }: { element: UMDTabsElement }) => {
-  const shadow = element.shadowRoot as ShadowRoot;
+const createComponent = (element: HTMLElement) => {
   const isThemeDark = Attributes.isTheme.dark({ element });
-  const topPosition = element.getAttribute(Attributes.names.LAYOUT_STICKY_TOP);
-  const slot = Node.slot({ type: Slots.name.TABS });
+  const slot = Markup.create.Node.slot({ type: Slots.name.TABS });
   const markup = element.querySelector(`[slot="${Slots.name.TABS}"]`);
 
   const modifyDom = () => {
@@ -40,65 +41,28 @@ const CreateShadowDom = ({ element }: { element: UMDTabsElement }) => {
     markup.innerHTML = '';
     markup.appendChild(updateDom);
   };
-  const createTabs = () => {
-    const tabsElement = Composite.tabs.standard.CreateElement({
-      isThemeDark,
-      tabsContainer: markup?.children[0] as HTMLElement,
-      shadowContent: slot,
-      topPosition,
-    });
-
-    if (!tabsElement) return;
-
-    element._elementRef = tabsElement;
-    shadow.appendChild(styleTemplate.content.cloneNode(true));
-    shadow.appendChild(tabsElement.element);
-    tabsElement.events.load();
-  };
 
   modifyDom();
-  createTabs();
+
+  return Composite.tabs.standard({
+    isThemeDark,
+    tabsContainer: markup?.children[0] as HTMLElement,
+    shadowContent: slot,
+    topPosition: Attributes.getValue.topPosition({
+      element,
+    }),
+  });
 };
 
-class UMDTabsElement extends HTMLElement {
-  _shadow: ShadowRoot;
-  _elementRef: {
-    element: HTMLDivElement;
-    events: {
-      resize: () => void;
-      load: () => void;
-    };
-  } | null;
-
-  constructor() {
-    super();
-    this._elementRef = null;
-    this._shadow = this.attachShadow({ mode: 'open' });
-  }
-
-  static get observedAttributes() {
-    return [Attributes.names.RESIZE];
-  }
-
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    if (!this._elementRef) return;
-
-    if (name === Attributes.names.RESIZE && newValue === 'true') {
-      this._elementRef.events.resize();
-    }
-  }
-
-  connectedCallback() {
-    CreateShadowDom({ element: this });
-  }
-}
-
 export default () => {
-  const hasElement =
-    document.getElementsByTagName(`${ELEMENT_NAME}`).length > 0;
-
-  if (!window.customElements.get(ELEMENT_NAME) && hasElement) {
-    window.UMDTabsElement = UMDTabsElement;
-    window.customElements.define(ELEMENT_NAME, UMDTabsElement);
-  }
+  Register.registerWebComponent({
+    name: tagName,
+    element: Model.createCustomElement({
+      tagName,
+      slots,
+      createComponent,
+      attributes,
+      afterConnect: (ref) => ref?.events?.load(),
+    }),
+  });
 };

565ea7aca7dd487b456b403f19eb40761f216624
Josh Magness <magnessjo@gmail.com>
2025-04-18 15:23:15 -0400

    Component - sticky columns; conversion to the element model

--- packages/components/source/api/layout/sticky-columns.ts
+++ packages/components/source/api/layout/sticky-columns.ts
@@ -1,91 +1,51 @@
-declare global {
-  interface Window {
-    UMDStickyColumnElement: typeof UMDStickyColumnElement;
-  }
-}
-
 import { Composite } from '@universityofmaryland/web-elements-library';
-import { Attributes, Slots } from 'model';
-import { Markup, Styles } from 'utilities';
+import { Attributes, Model, Register, Slots } from 'model';
+import { Markup } from 'utilities';
 
-const { Node } = Markup.create;
+const tagName = 'umd-element-sticky-columns';
 
-const ELEMENT_NAME = 'umd-element-sticky-columns';
-
-const styles = `
-  :host {
-    display: block;
-  }
-
-  ${Styles.reset}
-  ${Composite.layout.stickyColumns.Styles}
-`;
-
-export const CreateShadowDom = ({
-  element,
-}: {
-  element: UMDStickyColumnElement;
-}) => {
-  const isStickyLast = !Attributes.isVisual.stickyFirst({ element });
-  const topPosition = Attributes.getValue.topPosition({ element });
-
-  const stickyColumn = Node.slot({ type: Slots.name.STICKY_COLUMN });
-  const staticColumn = Node.slot({ type: Slots.name.STATIC_COLUMN });
-
-  const component = Composite.layout.stickyColumns.CreateElement({
-    stickyColumn,
-    staticColumn,
-    isStickyLast,
-    topPosition,
-  });
-
-  element._elementRef = component;
-  return component.element;
+const slots = {
+  [`sticky-column`]: {
+    required: true,
+  },
+  [`static-column`]: {
+    required: true,
+  },
 };
 
-export class UMDStickyColumnElement extends HTMLElement {
-  _shadow: ShadowRoot;
-  _elementRef: {
-    element: HTMLDivElement;
-    events: {
-      SetPosition: ({ value }: { value: string | null }) => void;
-    };
-  } | null;
+const attributes = Attributes.handler.combine(
+  Attributes.handler.observe.visuallyPosition({
+    callback: (element, topPosition) =>
+      element.events?.setPosition({ value: topPosition }),
+  }),
+  // Deprecated
+  Attributes.handler.observe.visuallyPosition({
+    name: Attributes.names.LAYOUT_STICKY_TOP,
+    callback: (element, topPosition) =>
+      element.events?.setPosition({ value: topPosition }),
+  }),
+);
 
-  constructor() {
-    const template = Node.stylesTemplate({ styles });
-
-    super();
-    this._shadow = this.attachShadow({ mode: 'open' });
-    this._shadow.appendChild(template.content.cloneNode(true));
-    this._elementRef = null;
-  }
-
-  static get observedAttributes() {
-    return [Attributes.names.LAYOUT_STICKY_TOP];
-  }
-
-  attributeChangedCallback(name: string) {
-    if (name == Attributes.names.LAYOUT_STICKY_TOP && this._elementRef) {
-      this._elementRef.events.SetPosition({
-        value: this.getAttribute(Attributes.names.LAYOUT_STICKY_TOP),
-      });
-    }
-  }
-
-  connectedCallback() {
-    const shadowNode = CreateShadowDom({ element: this });
-    if (!shadowNode) return;
-    this._shadow.appendChild(shadowNode);
-  }
-}
+const createComponent = (element: HTMLElement) =>
+  Composite.layout.stickyColumns({
+    stickyColumn: Markup.create.Node.slot({
+      type: Slots.name.STICKY_COLUMN,
+    }),
+    staticColumn: Markup.create.Node.slot({
+      type: Slots.name.STATIC_COLUMN,
+    }),
+    isStickyLast: !Attributes.isVisual.stickyFirst({ element }),
+    topPosition: Attributes.getValue.topPosition({ element }),
+  });
 
 export default () => {
-  const hasElement =
-    document.getElementsByTagName(`${ELEMENT_NAME}`).length > 0;
-
-  if (!window.customElements.get(ELEMENT_NAME) && hasElement) {
-    window.UMDStickyColumnElement = UMDStickyColumnElement;
-    window.customElements.define(ELEMENT_NAME, UMDStickyColumnElement);
-  }
+  Register.registerWebComponent({
+    name: tagName,
+    element: Model.createCustomElement({
+      tagName,
+      slots,
+      createComponent,
+      attributes,
+    }),
+  });
 };

c72993f5ba770cde2e8f5302600380de3274cb88
Mei Lu <meilu@umd.edu>
2025-05-07 15:32:32 -0400

    Fix(DSYS-2283) - Select chevron styling

--- packages/styles/source/element/field/select.ts
+++ packages/styles/source/element/field/select.ts
@@ -13,7 +13,6 @@ import { valid, invalid } from './_state';
  * Base64 encoded SVG for dark chevron down icon.
  * @private
  */
-const CHEVRON_DOWN_DARK = `data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iOTYiIGhlaWdodD0iOTYiIHZpZXdCb3g9IjAgMCA5NiA5NiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik00OC43NDAyIDUyLjE3OTRMMjMuNjY3NSAyNEwyIDI0LjAwMDFMMzcuMDkxNSA2NS4yNzE0TDM3LjA3NTMgNjUuMjg5N0w0OC43MTYgNzlMNDguNzQwMiA3OC45NzE1TDQ4Ljc2NDQgNzlMNjAuNDA1MSA2NS4yODk3TDYwLjM4ODkgNjUuMjcxNEw5NS40ODA0IDI0TDczLjgxMjkgMjQuMDAwMUw0OC43NDAyIDUyLjE3OTRaIiBmaWxsPSJibGFjayIvPgo8L3N2Zz4K`;
 
 /**
  * Base64 encoded SVG for red chevron down icon.
@@ -98,8 +97,9 @@ export const selectWrapper: JssObject = create.jss.objectWithClassName({
     zIndex: 9,
   },
 
-  [`&:after, &:before`]: {
+  '&:before': {
     content: "''",
+    backgroundImage: `url("${CHEVRON_DOWN_RED}")`,
     backgroundPosition: 'center center',
     backgroundSize: 'contain',
     backgroundRepeat: 'no-repeat',
@@ -108,28 +108,7 @@ export const selectWrapper: JssObject = create.jss.objectWithClassName({
     right: spacing.sm,
     top: `calc(50% - ${spacing.min})`,
     zIndex: 0,
-    transition: 'opacity 0.5s ease-in-out',
     height: spacing.sm,
     width: spacing.sm,
-  },
-
-  '&:before': {
-    backgroundImage: `url("${CHEVRON_DOWN_DARK}")`,
-    opacity: 1,
-  },
-
-  '&:after': {
-    backgroundImage: `url("${CHEVRON_DOWN_RED}")`,
-    opacity: 0,
-  },
-
-  [`&:hover, &:focus, &:focus-within`]: {
-    '&:before': {
-      opacity: 0,
-    },
-
-    '&:after': {
-      opacity: 1,
-    },
   },
 });

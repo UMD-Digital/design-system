6d18175de0b0ce0fe922f7e77e5766b0964d7272
Josh Magness <magnessjo@gmail.com>
2025-06-25 14:52:14 -0400

    Element - Hero display standard refactor
    Ref DSYS-2372

--- packages/elements/source/composite/hero/standard.ts
+++ packages/elements/source/composite/hero/standard.ts
@@ -1,349 +1,247 @@
-import {
-  element,
-  layout,
-  token,
-  typography,
-} from '@universityofmaryland/web-styles-library';
-import * as Utility from 'utilities';
-import ImageContainer, { TypeImageContainerProps } from './elements/image';
-import TextContainer, { TypeTextContainerProps } from './elements/text';
+import * as Styles from '@universityofmaryland/web-styles-library';
+import { assets, textLockup } from 'atomic';
+import { ElementModel } from 'model';
 
-type TypeHeroDefaultProps = TypeTextContainerProps &
-  TypeImageContainerProps & {
-    isInterior: boolean;
-    includesAnimation?: boolean;
-  };
+interface AnimationProps {
+  includesAnimation?: boolean;
+}
 
-const { convertJSSObjectToStyles } = Utility.styles;
+interface AssetProps extends AnimationProps {
+  image?: HTMLImageElement | null;
+  video?: HTMLVideoElement | null;
+}
 
-const TABLET = 768;
-const DESKTOP = 1024;
+interface TextProps extends AnimationProps {
+  headline?: HTMLElement | null;
+  eyebrow?: HTMLElement | null;
+  text?: HTMLElement | null;
+  actions?: HTMLElement | null;
+  isThemeDark?: boolean;
+  isTextCenter?: boolean;
+  isInterior: boolean;
+}
 
-const ATTRIBUTE_TEXT_ALIGN = 'text-align';
-const ATTRIBUTE_INTERIOR = 'interior';
-const ATTRIBUTE_HEADLINE_SIZE = 'headline-size';
-const ATTRIBUTE_IS_VIDEO = 'data-video';
-const ATTRIBUTE_ANIMATION = 'data-animation';
-const TEXT_ALIGN_CENTER = 'center';
-const HEADLINE_SIZE_LARGE = 'extra-large';
+interface HeroStandardProps extends AssetProps, TextProps {}
 
-const ELEMENT_NAME = 'umd-element-hero-default';
-const HERO_ELEMENT_DECLARATION = 'hero-default-element-declaration';
-const HERO_CONTAINER = 'hero-default-container';
-const HERO_LOCK = 'hero-default-lock';
-
-const IS_TEXT_CENTER = `[${ATTRIBUTE_TEXT_ALIGN}='${TEXT_ALIGN_CENTER}']`;
-const IS_INTERIOR = `[${ATTRIBUTE_INTERIOR}]`;
-const IS_LARGE_HEADLINE = `[${ATTRIBUTE_HEADLINE_SIZE}='${HEADLINE_SIZE_LARGE}']`;
-const IS_ANIMATION = `[${ATTRIBUTE_ANIMATION}]`;
-
-const OVERWRITE_TEXT_CONTAINER = `.${HERO_CONTAINER} .${TextContainer.Elements.container}`;
-const OVERWRITE_IMAGE_CONTAINER = `.${HERO_CONTAINER} .${ImageContainer.Elements.container}`;
-const OVERWRITE_EYEBROW = `.${HERO_CONTAINER} .${TextContainer.Elements.eyebrow}`;
-const OVERWRITE_HEADLINE = `.${HERO_CONTAINER} .${TextContainer.Elements.headline}`;
-const OVERWRITE_RICH_TEXT = `.${HERO_CONTAINER} .${TextContainer.Elements.richText}`;
-
-const OVERWRITE_INTERIOR_CONTAINER = `.${HERO_CONTAINER}${IS_INTERIOR}`;
-const OVERWRITE_TEXT_CENTER_ALIGNMENT_TEXT_CONTAINER = `.${HERO_CONTAINER}${IS_TEXT_CENTER} .${TextContainer.Elements.container}`;
-const OVERWRITE_SIZE_LARGE_HEADLINE = `.${HERO_CONTAINER}${IS_LARGE_HEADLINE} .${TextContainer.Elements.headline}`;
-
-const OVERWRITE_VIDEO_CONTAINER = `.${HERO_CONTAINER}[${ATTRIBUTE_IS_VIDEO}]`;
-const OVERWRITE_VIDEO_IMAGE_WRAPPER = `${OVERWRITE_VIDEO_CONTAINER} .${TextContainer.Elements.wrapper}`;
-
-const OVERWRITE_CONTAINER_ANIMATION = `.${HERO_CONTAINER}${IS_ANIMATION}`;
-const OVERWRITE_IMAGE_WRAPPER_ANIMATION = `${OVERWRITE_CONTAINER_ANIMATION} .${TextContainer.Elements.wrapper}`;
-const OVERWRITE_IMAGE_CONTAINER_ANIMATION = `${OVERWRITE_CONTAINER_ANIMATION} .${ImageContainer.Elements.container}`;
-
-// prettier-ignore
-const OverwriteEyebrow = `
-  ${convertJSSObjectToStyles({
-    styleObj: {
-      [`${OVERWRITE_EYEBROW}`]: element.text.decoration.ribbon,
-    },
-  })}
-
-  ${OVERWRITE_EYEBROW} {
-    color: ${token.color.black} !important;
-  }
-`;
-
-// prettier-ignore
-const OverwriteHeadline = `
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    ${OVERWRITE_HEADLINE} {
-      max-width: 700px;
-      margin: 0 auto;
-      color: ${token.color.white};
-    }
-  }
-  
-  @container ${ELEMENT_NAME} (min-width: ${DESKTOP}px) {
-    ${OVERWRITE_HEADLINE} {
-      max-width: 816px;
-    }
-  }
-  
-  ${convertJSSObjectToStyles({
-    styleObj: {
-      [`${OVERWRITE_HEADLINE}`]: typography.campaign.extralarge,
-    },
-  })}
-
-  @container ${ELEMENT_NAME} (min-width: ${DESKTOP}px) {
-    ${convertJSSObjectToStyles({
-      styleObj: {
-        [`${OVERWRITE_SIZE_LARGE_HEADLINE}`]: typography.campaign.maxium,
-      },
-    })}
-  }
-`;
-
-// prettier-ignore
-const OverwriteRichText = `
-  ${convertJSSObjectToStyles({
-    styleObj: {
-      [`${OVERWRITE_RICH_TEXT}`]: typography.sans.larger,
-    },
-  })}
-
-  ${convertJSSObjectToStyles({
-    styleObj: {
-      [`${OVERWRITE_RICH_TEXT} *`]: typography.sans.larger,
-    },
-  })}
-  
-  ${OVERWRITE_RICH_TEXT} {
-    font-weight: 400;
-  }
-`;
-
-// prettier-ignore
-const OverwriteTextContainer = `
-  ${OVERWRITE_TEXT_CONTAINER} {
-    display: flex;
-    align-items: flex-end;
-  }
-
-  @container ${ELEMENT_NAME} (max-width: ${TABLET - 1}px) {
-    ${OVERWRITE_TEXT_CONTAINER} {
-      padding-top: ${token.spacing.sm};
-    }
-  }
-
-  @container ${ELEMENT_NAME} (max-width: ${TABLET - 1}px) {
-    ${OVERWRITE_TEXT_CONTAINER}:has(.${TextContainer.Elements.eyebrow}) {
-      margin-top: -14px;
-      padding-top: 0;
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    ${OVERWRITE_TEXT_CONTAINER} {
-      max-width: 736px;
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${DESKTOP}px) {
-    ${OVERWRITE_TEXT_CONTAINER} {
-      max-width: 808px;
-    }
-  }
-
-  ${OVERWRITE_TEXT_CENTER_ALIGNMENT_TEXT_CONTAINER} {
-    justify-content: center;
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 928px;
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    ${OVERWRITE_TEXT_CENTER_ALIGNMENT_TEXT_CONTAINER} {
-      width: 80%;
-    }
-  }
-`;
-
-// prettier-ignore
-const OverwriteImageContainer = `
-  @container ${ELEMENT_NAME} (max-width: ${TABLET - 1}px) {
-    ${OVERWRITE_IMAGE_CONTAINER} {
-      aspect-ratio: 16 / 9;
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    ${OVERWRITE_IMAGE_CONTAINER} {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    ${OVERWRITE_IMAGE_CONTAINER}:before {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .8) 85%);
-      z-index: 99;
-    }
-  }
-
-  ${OVERWRITE_IMAGE_CONTAINER} img,
-  ${OVERWRITE_IMAGE_CONTAINER} video {
-    object-fit: cover;
-    object-position: center;
-    height: 100%;
-    width: 100%;
-  }
-`;
-
-const AnimationStyles = `
-  @keyframes hero-scale-down {
-    from { transform: scale(1.1); }
-    to { transform: scale(1); }
-  }
-
+const keyFrameHeroSlideUp = `
   @keyframes hero-slide-up {
-    from { transform: translateY(25px); opacity: .2 }
-    to { transform: translateY(0); opacity: 1 }
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    ${OVERWRITE_IMAGE_CONTAINER_ANIMATION} img {
-      animation: hero-scale-down forwards 1s;
+    from {
+      transform: translateY(25px);
+      opacity: .2;
     }
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    ${OVERWRITE_IMAGE_WRAPPER_ANIMATION} {
-      animation: hero-slide-up forwards 1s;
-    }
-  }
-
-  ${OVERWRITE_VIDEO_IMAGE_WRAPPER} {
-    animation: none;
-  }
-`;
-
-// prettier-ignore
-const DefaultStyles = `
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    .${HERO_CONTAINER} {
-      height: 75vh;
-      min-height: 480px;
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${DESKTOP}px) {
-    .${HERO_CONTAINER} {
-      min-height: 720px;
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    ${OVERWRITE_INTERIOR_CONTAINER} {
-      min-height: auto;
-      height: auto;
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    .${HERO_CONTAINER} * {
-      color: ${token.color.white};
-    }
-  }
-
-  .${HERO_LOCK} {
-    height: 100%;
-    width: 100%;
-    position: relative;
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    .${HERO_CONTAINER} .${HERO_LOCK} {
-      padding-top: ${token.spacing['2xl']};
-      padding-bottom: ${token.spacing['2xl']};
-    }
-  }
-
-  @container ${ELEMENT_NAME} (min-width: ${TABLET}px) {
-    ${OVERWRITE_INTERIOR_CONTAINER} .${HERO_LOCK} {
-      min-height: 400px;
-      align-items: flex-end;
-      display: flex;
+    to {
+      transform: translateY(0);
+      opacity: 1;
     }
   }
 `;
 
-export const STYLES_HERO_DEFAULT_ELEMENT = `
-  .${HERO_ELEMENT_DECLARATION} {
-    container: ${ELEMENT_NAME} / inline-size;
-    position: relative;
-    overflow: hidden;
+const keyFrameHeroScaleDown = `
+  @keyframes hero-scale-down {
+    from {
+      transform: scale(1.1);
+    }
+    to {
+      transform: scale(1);
+    }
   }
-  
-  ${convertJSSObjectToStyles({
-    styleObj: {
-      [`.${HERO_LOCK}`]: layout.space.horizontal.max,
+`;
+
+const createAsset = ({ image, video, includesAnimation }: AssetProps) => {
+  const assetContainer = ElementModel.create({
+    element: document.createElement('div'),
+    className: 'umd-hero-default__asset',
+    elementStyles: {
+      element: {
+        [`@container (${Styles.token.media.queries.large.max})`]: {
+          aspectRatio: '16 / 9',
+        },
+        [`@container (${Styles.token.media.queries.tablet.min})`]: {
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+        },
+
+        [`&:before`]: {
+          [`@container (${Styles.token.media.queries.tablet.min})`]: {
+            content: '""',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            background:
+              'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .8) 85%)',
+            zIndex: '99',
+          },
+        },
+
+        ['& img']: {
+          ...(includesAnimation && {
+            animation: 'hero-scale-down forwards 1s',
+          }),
+        },
+      },
     },
-  })}
-  
+  });
 
-  ${OverwriteEyebrow}
-  ${OverwriteHeadline}
-  ${OverwriteRichText}
-  ${DefaultStyles}
-  ${AnimationStyles}
-  ${OverwriteImageContainer}
-  ${OverwriteTextContainer}
-`;
+  if (video && video instanceof HTMLVideoElement) {
+    const videoElement = assets.video.observedAutoPlay({ video });
+    assetContainer.element.appendChild(videoElement.element);
+    assetContainer.styles += videoElement.styles;
 
-export default (element: TypeHeroDefaultProps) => {
-  const { headline, isTextCenter, isInterior, videoRef, includesAnimation } =
-    element;
-  const declaration = document.createElement('div');
-  const container = document.createElement('div');
-  const lock = document.createElement('div');
-  const text = TextContainer.CreateElement(element);
-  const asset = ImageContainer.CreateElement(element);
-  let styles = STYLES_HERO_DEFAULT_ELEMENT;
-
-  lock.classList.add(HERO_LOCK);
-  lock.appendChild(text.element);
-  styles += text.styles;
-
-  if (asset) {
-    container.appendChild(asset.element);
-    styles += asset.styles;
+    return assetContainer;
   }
 
-  if (videoRef) container.setAttribute(ATTRIBUTE_IS_VIDEO, '');
-
-  if (!isInterior && headline) {
-    const characterCount = headline.textContent?.trim().length || 0;
-    if (characterCount < 30) {
-      container.setAttribute(ATTRIBUTE_HEADLINE_SIZE, HEADLINE_SIZE_LARGE);
-    }
+  if (image) {
+    const imageElement = assets.image.background({
+      image,
+      isScaled: true,
+      isShowCaption: true,
+    });
+    assetContainer.element.appendChild(imageElement.element);
+    assetContainer.styles += imageElement.styles;
+    return assetContainer;
   }
 
-  if (isTextCenter)
-    container.setAttribute(ATTRIBUTE_TEXT_ALIGN, TEXT_ALIGN_CENTER);
-  if (isInterior) container.setAttribute(ATTRIBUTE_INTERIOR, '');
-  if (includesAnimation) container.setAttribute(ATTRIBUTE_ANIMATION, '');
-  container.classList.add(HERO_CONTAINER);
-  container.appendChild(lock);
+  const defaultImage = assets.image.placeholder.fearlessForward();
+  assetContainer.element.appendChild(defaultImage.element);
+  assetContainer.styles += defaultImage.styles;
 
-  declaration.classList.add(HERO_ELEMENT_DECLARATION);
-  declaration.appendChild(container);
-
-  return {
-    element: declaration,
-    styles,
-  };
+  return assetContainer;
 };
+
+const createText = (props: TextProps) => {
+  const { headline, isTextCenter, isInterior, includesAnimation } = props;
+  const characterCount = headline?.textContent?.trim().length || 0;
+  const lock = ElementModel.layout.spaceHorizontalMax({
+    element: document.createElement('div'),
+    elementStyles: {
+      element: {
+        height: '100%',
+        width: '100%',
+        position: 'relative',
+        zIndex: 99,
+
+        [`@container (${Styles.token.media.queries.tablet.max})`]: {
+          paddingTop: `${Styles.token.spacing.sm}`,
+
+          [`&:has(.${Styles.element.text.decoration.ribbon.className})`]: {
+            paddingTop: `0`,
+            marginTop: '-14px',
+          },
+        },
+      },
+    },
+  });
+
+  const textContainer = ElementModel.create({
+    element: document.createElement('div'),
+    className: 'umd-hero-default__text',
+    elementStyles: {
+      element: {
+        display: 'flex',
+        alignItems: 'flex-end',
+        height: '100%',
+        ...(isTextCenter && {
+          textAlign: 'center',
+          justifyContent: 'center',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '928px',
+        }),
+
+        ...(includesAnimation && {
+          animation: 'hero-slide-up forwards 1s',
+        }),
+
+        [`@container (${Styles.token.media.queries.tablet.min})`]: {
+          maxWidth: '736px',
+          paddingTop: `${Styles.token.spacing['2xl']}`,
+          paddingBottom: `${Styles.token.spacing['2xl']}`,
+          ...(!isTextCenter && {
+            width: '80%',
+          }),
+          ...(isInterior && {
+            minHeight: '400px',
+            alignItems: 'flex-end',
+            display: 'flex',
+          }),
+        },
+
+        [`@container (${Styles.token.media.queries.desktop.min})`]: {
+          maxWidth: '808px',
+        },
+      },
+    },
+  });
+
+  const isOverwriteHeadline = characterCount < 30 && !isInterior;
+  const text = textLockup.large({
+    ...props,
+    isHeadlineMaximum: isOverwriteHeadline,
+    isThemeDark: true,
+  });
+
+  textContainer.element.appendChild(text.element);
+  textContainer.styles += text.styles;
+
+  lock.element.appendChild(textContainer.element);
+  lock.styles += textContainer.styles;
+
+  return lock;
+};
+
+export default (props: HeroStandardProps) =>
+  (() => {
+    const { isInterior } = props;
+
+    const composite = ElementModel.create({
+      element: document.createElement('div'),
+      className: 'umd-hero-default',
+      elementStyles: {
+        element: {
+          position: 'relative',
+          overflow: 'hidden',
+          [`@container (${Styles.token.media.queries.tablet.min})`]: {
+            ...(!isInterior && {
+              height: '75vh',
+              minHeight: '480px',
+            }),
+          },
+
+          [`@container (${Styles.token.media.queries.desktop.min})`]: {
+            ...(!isInterior && {
+              minHeight: '720px',
+            }),
+          },
+        },
+        subElement: {
+          [`@container (${Styles.token.media.queries.large.max})`]: {
+            ['*']: {
+              color: `${Styles.token.color.black} !important`,
+            },
+          },
+        },
+      },
+    });
+
+    const asset = createAsset(props);
+    const text = createText(props);
+
+    composite.element.appendChild(asset.element);
+    composite.styles += asset.styles;
+    composite.element.appendChild(text.element);
+    composite.styles += text.styles;
+
+    composite.styles = `
+      ${keyFrameHeroScaleDown}
+      ${keyFrameHeroSlideUp}
+      ${composite.styles}
+    `;
+
+    return composite;
+  })();

daa04793db6ddd1697fd774471dda5ef7093c6fd
Josh Magness <magnessjo@gmail.com>
2025-07-24 15:21:45 -0400

    Element - card overlay color: element model syntax
    Ref DSYS-2466

--- packages/elements/source/composite/card/overlay/color.ts
+++ packages/elements/source/composite/card/overlay/color.ts
@@ -2,64 +2,67 @@ import * as Styles from '@universityofmaryland/web-styles-library';
 import { actions, textLockup } from 'atomic';
 import { ElementModel } from 'model';
 import { CardOverlayProps } from '../_types';
-
-const elementStyles = {
-  element: {
-    className: 'card-overlay-color',
-    containerType: 'inline-size',
-
-    [`& .${Styles.layout.grid.inline.tabletRows.className}`]: {
-      [`@media (${Styles.token.media.queries.tablet.min})`]: {
-        marginTop: 'auto !important',
-      },
-    },
-  },
-};
+import { ElementVisual } from '../../../_types';
 
 export default (props: CardOverlayProps) => {
   const { isThemeDark, ctaIcon } = props;
 
-  const composite = ElementModel.composite.card.overlay.color({
-    ...props,
-    element: document.createElement('div'),
-    elementStyles,
-  });
-  const wrapper = ElementModel.create({
-    element: document.createElement('div'),
-    className: 'card-overlay-color-wrapper',
+  const children: ElementVisual[] = [];
+
+  if (ctaIcon && ctaIcon instanceof HTMLElement) {
+    children.push(
+      actions.icon({
+        ...props,
+        ctaIcon,
+        isThemeLight: !isThemeDark,
+      }),
+    );
+  }
+
+  children.push(
+    ElementModel.createDiv({
+      className: 'card-overlay-color-wrapper',
+      elementStyles: {
+        element: {
+          paddingRight: `${ctaIcon ? Styles.token.spacing['2xl'] : 0}`,
+          height: '100%',
+
+          [`& > *`]: {
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            zIndex: 9,
+            position: 'relative',
+            maxWidth: `${Styles.token.spacing.maxWidth.smallest}`,
+          },
+        },
+      },
+      children: [textLockup.smallScaling(props)],
+    }),
+  );
+
+  return ElementModel.createDiv({
+    className: 'card-overlay-color',
+    children,
     elementStyles: {
       element: {
-        paddingRight: `${ctaIcon ? Styles.token.spacing['2xl'] : 0}`,
+        padding: `${Styles.token.spacing.lg} ${Styles.token.spacing.md}`,
+        minHeight: '360px',
         height: '100%',
+        overflow: 'hidden',
+        position: 'relative',
+        backgroundColor: Styles.token.color.gray.lightest,
 
-        [`& > *`]: {
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          zIndex: 9,
-          position: 'relative',
+        ...(isThemeDark && {
+          backgroundColor: Styles.token.color.gray.darker,
+        }),
+
+        [`& .${Styles.layout.grid.inline.tabletRows.className}`]: {
+          [`@media (${Styles.token.media.queries.tablet.min})`]: {
+            marginTop: 'auto !important',
+          },
         },
       },
     },
   });
-  const scalingFontContainer = textLockup.smallScaling(props);
-
-  if (ctaIcon && ctaIcon instanceof HTMLElement) {
-    const actionIcon = actions.icon({
-      ...props,
-      ctaIcon,
-      isThemeLight: !isThemeDark,
-    });
-
-    composite.element.appendChild(actionIcon.element);
-    composite.styles += actionIcon.styles;
-  }
-
-  wrapper.element.appendChild(scalingFontContainer.element);
-  wrapper.styles += scalingFontContainer.styles;
-
-  composite.element.appendChild(wrapper.element);
-  composite.styles += wrapper.styles;
-
-  return composite;
 };

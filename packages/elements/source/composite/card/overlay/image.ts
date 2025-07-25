f7b7b6ae93a6fa54b4076d8d2df7d41f9f3e6612
Josh Magness <magnessjo@gmail.com>
2025-07-25 12:45:31 -0400

    Element - Card Overlay: element model syntax
    Ref  DSYS-2466

--- packages/elements/source/composite/card/overlay/image.ts
+++ packages/elements/source/composite/card/overlay/image.ts
@@ -1,106 +1,19 @@
 import * as Styles from '@universityofmaryland/web-styles-library';
-import { actions, assets, textLockup } from 'atomic';
 import { ElementModel } from 'model';
-import * as Utilities from 'utilities';
+import { actions, assets, textLockup } from 'atomic';
+import { asset, markup, theme } from 'utilities';
 import { CardOverlayProps } from '../_types';
-
-const elementStyles: Record<string, any> = {
-  element: {
-    className: 'card-block-overall-container',
-    containerType: 'inline-size',
-    height: '100%',
-  },
-};
+import { ElementVisual } from '../../../_types';
 
 export default (props: CardOverlayProps) => {
   const { isQuote, ctaIcon, dateSign, backgroundImage, text } = props;
-
-  const composite = ElementModel.composite.card.overlay.image({
-    ...props,
-    element: document.createElement('div'),
-    elementStyles,
-    isThemeDark: true,
-  });
-  const wrapper = ElementModel.create({
-    element: document.createElement('div'),
-    className: 'card-overlay-image-wrapper',
-    elementStyles: {
-      element: {
-        paddingRight: `${ctaIcon ? Styles.token.spacing['2xl'] : 0}`,
-      },
-    },
-  });
-  let imageContainerClass: string | null = null;
-
-  if (ctaIcon && ctaIcon instanceof HTMLElement) {
-    const actionIcon = actions.icon({ ...props, ctaIcon, isThemeLight: false });
-
-    composite.element.appendChild(actionIcon.element);
-    composite.styles += actionIcon.styles;
-  }
-
-  if (backgroundImage) {
-    const imageContainer = assets.image.background({
-      image: backgroundImage,
-      isScaled: true,
-    });
-
-    composite.element.appendChild(imageContainer.element);
-    composite.styles += imageContainer.styles;
-    imageContainerClass = imageContainer.element.className;
-  }
-
-  const textLockupElement = textLockup.smallScaling({
-    ...props,
-    isThemeDark: true,
-  });
-
-  if (dateSign) {
-    const dataWrapper = ElementModel.layout.backgroundBoxWhite({
-      element: document.createElement('div'),
-      elementStyles: {
-        element: {
-          alignSelf: `flex-start`,
-        },
-        siblingAfter: {
-          marginTop: `${Styles.token.spacing.min}`,
-        },
-      },
-    });
-    dataWrapper.element.appendChild(dateSign.element);
-
-    textLockupElement.element.insertBefore(
-      dataWrapper.element,
-      textLockupElement.element.firstChild,
-    );
-    composite.styles += dataWrapper.styles;
-    composite.styles += dateSign.styles;
-  }
-
-  if (isQuote) {
-    const quoteWrapper = ElementModel.composite.card.overlay.elementQuote({
-      element: document.createElement('div'),
-    });
-
-    quoteWrapper.element.innerHTML = Utilities.asset.icon.QUOTE;
-    textLockupElement.element.insertBefore(
-      quoteWrapper.element,
-      textLockupElement.element.firstChild,
-    );
-    composite.styles += quoteWrapper.styles;
-  }
-
-  wrapper.element.appendChild(textLockupElement.element);
-  wrapper.styles += textLockupElement.styles;
-
-  composite.element.appendChild(wrapper.element);
-  composite.styles += wrapper.styles;
-
+  const children: ElementVisual[] = [];
+  const wrapperChildren: ElementVisual[] = [];
   const load = () => {
     const sizeElements = () => {
       const textCopy = text?.innerHTML;
       if (text && textCopy) {
-        const modifiedText = Utilities.markup.modify.truncateTextBasedOnSize({
+        const modifiedText = markup.modify.truncateTextBasedOnSize({
           text: textCopy,
           size: composite.element.offsetWidth,
         });
@@ -120,6 +33,157 @@ export default (props: CardOverlayProps) => {
 
     sizeElements();
   };
+  let imageContainerClass: string | null = null;
+
+  if (ctaIcon && ctaIcon instanceof HTMLElement) {
+    children.push(actions.icon({ ...props, ctaIcon, isThemeLight: false }));
+  }
+
+  if (backgroundImage) {
+    const imageContainer = assets.image.background({
+      image: backgroundImage,
+      isScaled: true,
+      customStyles: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+
+        [`&:before`]: {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 1,
+          opacity: 1,
+          transition: `opacity 0.5s ease-in-out`,
+          background:
+            'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .55) 60%, rgba(0, 0, 0, 0.9) 100%)',
+        },
+
+        [`&[data-size="large"]:before`]: {
+          background:
+            'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .55) 30%, rgba(0, 0, 0, 0.9) 100%)',
+        },
+
+        [`img`]: {
+          transform: 'scale(1.025)',
+        },
+      },
+    });
+    children.push(imageContainer);
+    imageContainerClass = imageContainer.element.className;
+  }
+
+  if (dateSign) {
+    wrapperChildren.push(
+      ElementModel.layout.backgroundBoxWhite({
+        element: document.createElement('div'),
+        elementStyles: {
+          element: {
+            alignSelf: `flex-start`,
+          },
+          siblingAfter: {
+            marginTop: `${Styles.token.spacing.min}`,
+          },
+        },
+        children: [dateSign],
+      }),
+    );
+  }
+
+  if (isQuote) {
+    const quoteWrapper = ElementModel.createDiv({
+      className: 'card-overlay-image-quote-wrapper',
+      elementStyles: {
+        element: {
+          width: '41px',
+          height: '30px',
+          marginBottom: `${Styles.token.spacing.xs}`,
+
+          '& svg': {
+            fill: `${Styles.token.color.red}`,
+          },
+        },
+      },
+    });
+
+    quoteWrapper.element.innerHTML = asset.icon.QUOTE;
+    wrapperChildren.push(quoteWrapper);
+  }
+
+  wrapperChildren.push(
+    textLockup.smallScaling({
+      ...props,
+      isThemeDark: true,
+    }),
+  );
+
+  const textWrapper = ElementModel.createDiv({
+    className: 'card-overlay-image-wrapper',
+    elementStyles: {
+      element: {
+        height: 'auto',
+        paddingRight: `${ctaIcon ? Styles.token.spacing['2xl'] : 0}`,
+      },
+    },
+    children: [
+      ElementModel.createDiv({
+        children: wrapperChildren,
+        className: 'card-overlay-image-text-content',
+        elementStyles: {
+          element: {
+            maxWidth: `${Styles.token.spacing.maxWidth.smallest}`,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            zIndex: 9,
+            position: 'relative',
+          },
+        },
+      }),
+    ],
+  });
+
+  const composite = ElementModel.createDiv({
+    className: 'card-overlay-image',
+    children: [...children, textWrapper],
+    elementStyles: {
+      element: {
+        overflow: 'hidden',
+        position: 'relative',
+        minHeight: '360px',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        padding: `${Styles.token.spacing.lg} ${Styles.token.spacing.md}`,
+        paddingTop: `${Styles.token.spacing['4xl']}`,
+
+        ...theme.media.createContainerQuery(
+          'min-width',
+          Styles.token.media.breakpointValues.medium.min,
+          {
+            paddingTop: `${Styles.token.spacing['8xl']}`,
+            minHeight: `424px`,
+          },
+        ),
+
+        [`&:hover .${imageContainerClass}:before, &:focus .${imageContainerClass}:before`]:
+          {
+            opacity: 0.7,
+          },
+
+        ['&:hover img, &:focus img']: {
+          transform: 'scale(1)',
+          transition: 'transform 0.5s ease-in-out',
+        },
+      },
+    },
+  });
 
   return {
     ...composite,
--- packages/elements/source/model/elements/composite/card/index.ts
+++
@@ -1,1 +0,0 @@
-export * as overlay from './overlay';

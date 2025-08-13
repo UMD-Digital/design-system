19d1f04c140a5b7ba49251abbdae3d66bddcdfdf
Josh Magness <magnessjo@gmail.com>
2025-08-13 11:08:57 -0400

    Atomic - Text Lockup Medium
    Ref DSYS-2424

--- packages/elements/source/atomic/text-lockup/index.ts
+++ packages/elements/source/atomic/text-lockup/index.ts
@@ -1,6 +1,7 @@
 export { default as contact } from './contact';
 export { default as date } from './date';
 export { default as large } from './large';
+export { default as medium } from './medium';
 export { default as person } from './person';
 export { default as small } from './small';
 export { default as smallScaling } from './small-scaling';

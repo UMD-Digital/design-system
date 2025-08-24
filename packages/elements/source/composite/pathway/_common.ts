c5cbbc3cf3390219eb943dd28b39a8beecbe171b
Josh Magness <magnessjo@gmail.com>
2025-08-24 14:57:12 -0400

    Element - Pathway: Overlay; Element Model conversion
    Ref DSYS-2427

---
+++ packages/elements/source/composite/pathway/_common.ts
@@ -0,0 +1,151 @@
+import * as Styles from '@universityofmaryland/web-styles-library';
+import { ElementModel } from 'model';
+import * as Atomic from 'atomic';
+import { type PathwayTextLockupProps, type PathwayAssetProps } from './_types';
+
+export const createCompositeHeadline = ({
+  headline,
+  isThemeDark,
+  isThemeMaryland,
+}: Pick<
+  PathwayTextLockupProps,
+  'headline' | 'isThemeDark' | 'isThemeMaryland'
+>) => {
+  const characterCount = headline?.textContent?.trim().length || 0;
+  const isOverwriteHeadline = characterCount > 30;
+
+  if (!headline) return null;
+
+  const desktopStyles = {
+    [`@container (${Styles.token.media.queries.desktop.min})`]: {
+      ...(isOverwriteHeadline && {
+        fontSize: '40px',
+      }),
+    },
+  };
+
+  return ElementModel.headline.sansLargest({
+    element: headline,
+    isThemeDark: isThemeDark || isThemeMaryland,
+    elementStyles: {
+      element: {
+        fontWeight: 800,
+        textTransform: 'uppercase',
+        textWrap: 'balance',
+        ...desktopStyles,
+      },
+      siblingAfter: {
+        marginTop: Styles.token.spacing.md,
+      },
+    },
+  });
+};
+
+export const createCompositeStat = ({
+  stats,
+}: Pick<PathwayTextLockupProps, 'stats'>) => {
+  const mediumSize = 800;
+
+  if (!stats) return null;
+
+  const statWrapper = ElementModel.createDiv({
+    className: 'text-lockup-medium-stats',
+    elementStyles: {
+      element: {
+        marginTop: Styles.token.spacing.lg,
+
+        [`&:has(> *:nth-child(2))`]: {
+          display: `grid`,
+          gridGap: `${Styles.token.spacing.md}`,
+        },
+
+        [`@container (max-width: ${mediumSize - 1}px)`]: {
+          marginTop: Styles.token.spacing.lg,
+          paddingTop: Styles.token.spacing.md,
+          borderTop: `1px solid ${Styles.token.color.gray.light}`,
+        },
+
+        [`@container (min-width: ${mediumSize}px)`]: {
+          marginTop: Styles.token.spacing['2xl'],
+
+          [`&:has(> *:nth-child(2))`]: {
+            gridGap: `${Styles.token.spacing.lg}`,
+            gridTemplateColumns: `repeat(2, 1fr)`,
+          },
+        },
+      },
+    },
+  });
+
+  statWrapper.element.innerHTML = stats.innerHTML;
+
+  return statWrapper;
+};
+
+export const createTextLockupMedium = (
+  props: Pick<
+    PathwayTextLockupProps,
+    | 'stats'
+    | 'actions'
+    | 'eventDetails'
+    | 'eyebrow'
+    | 'headline'
+    | 'stats'
+    | 'text'
+    | 'isThemeDark'
+    | 'isThemeMaryland'
+  >,
+) =>
+  Atomic.textLockup.medium({
+    ...props,
+    compositeHeadline: createCompositeHeadline(props),
+    compositeStats: createCompositeStat(props),
+  });
+
+export const createAssetContent = (
+  props: Pick<
+    PathwayAssetProps,
+    'dateSign' | 'image' | 'video' | 'isImageScaled'
+  >,
+) => {
+  const { dateSign, image, video, isImageScaled } = props;
+  const children = [];
+
+  if (video) {
+    children.push(
+      Atomic.assets.video.observedAutoPlay({
+        video,
+        isScaled: isImageScaled,
+      }),
+    );
+  }
+
+  if (!video && image) {
+    children.push(
+      Atomic.assets.image.background({
+        image,
+        isScaled: isImageScaled,
+        isShowCaption: true,
+        dateSign,
+      }),
+    );
+  }
+
+  return ElementModel.createDiv({
+    className: 'pathway-image-container-wrapper',
+    children,
+    elementStyles: {
+      element: {
+        position: 'relative',
+        overflow: 'hidden',
+        height: '100%',
+        width: '100%',
+        display: 'grid',
+
+        [`&:has(.image-container)`]: {
+          alignItems: 'center',
+        },
+      },
+    },
+  });
+};

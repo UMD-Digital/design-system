55f471e90f22471773e3a5f8973ccfe8b6d58697
Josh Magness <magnessjo@gmail.com>
2025-09-07 15:53:13 -0400

    Elements - name space old directory to layout for reference errors
    Ref DSYS-2564

--- packages/elements/source/index.ts
+++ packages/elements/source/index.ts
@@ -1,5 +1,5 @@
 export * as Atomic from './atomic';
 export * as Composite from './composite';
-export * as Layout from './old';
+export * as Layout from './layout';
 export * as Model from './model';
 export * as Utilities from './utilities';

0e4350b12734d42d12d715f7d51b9a9fb4d1cc6b
Josh Magness <magnessjo@gmail.com>
2025-10-25 14:09:44 -0400

    Elements - Composite: Carousel; Conversion to ElementBuilder
    Ref DSYS-2503

--- packages/elements/source/composite/carousel/wide/_elementModel.ts
+++ packages/elements/source/composite/carousel/wide/_elementModel.ts
@@ -1,10 +1,10 @@
-import ElementBuilder from '@universityofmaryland/web-builder-library';
-import { type ElementVisual } from '../../../_types';
+import { ElementBuilder } from '@universityofmaryland/web-builder-library';
+import { type UMDElement } from '../../../_types';
 
 interface ElementWithRefsProps<T extends Record<string, any>> {
   className: string;
   element?: HTMLElement;
-  children?: ElementVisual[];
+  children?: UMDElement[];
   elementStyles?: any;
   attributes?: Record<string, string>[];
   refs: T;
@@ -20,17 +20,34 @@ export interface ElementWithRefsReturn<T extends Record<string, any>> {
 export function createElementWithRefs<T extends Record<string, any>>(
   props: ElementWithRefsProps<T>,
 ): ElementWithRefsReturn<T> {
-  const { refs, element, ...elementProps } = props;
+  const { refs, element, children, elementStyles, attributes, className } =
+    props;
 
-  const component = element
-    ? ElementBuilder.create.element({
-        element,
-        ...elementProps,
-      })
-    : ElementBuilder.create.div(elementProps);
+  const builder = element ? new ElementBuilder(element) : new ElementBuilder();
+
+  builder.withClassName(className);
+
+  if (children) {
+    builder.withChildren(...children);
+  }
+
+  if (elementStyles) {
+    builder.withStyles(elementStyles);
+  }
+
+  if (attributes) {
+    attributes.forEach((attr) => {
+      const [key, value] = Object.entries(attr)[0];
+      builder.withAttribute(key, value);
+    });
+  }
+
+  const component = builder.build();
 
   return {
-    ...component,
+    element: component.element,
+    className,
+    styles: component.styles,
     refs,
   };
 }

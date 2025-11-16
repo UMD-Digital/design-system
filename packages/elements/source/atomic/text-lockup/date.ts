42247e2ef049810d52b874b89629b94702765f1c
Josh Magness <magnessjo@gmail.com>
2025-11-16 15:44:54 -0500

    Element - atomic: event title theme color
    Ref DSYS-2737

--- packages/elements/source/atomic/text-lockup/date.ts
+++ packages/elements/source/atomic/text-lockup/date.ts
@@ -55,6 +55,10 @@ const HeadlineStyles = `
       [`.${EVENT_DATE_CONTAINER}${IS_THEME_DARK} .${EVENT_HEADLINE} a`]: animation.line.slideUnderWhite,
     },
   })}
+
+  .${EVENT_DATE_CONTAINER}${IS_THEME_DARK} .${EVENT_HEADLINE} * { 
+    color: ${token.color.white};
+  }
 `;
 
 // prettier-ignore

0cb4aeb328204866d754596ed1d40c59b74900e9
Josh Magness <magnessjo@gmail.com>
2025-12-15 15:55:01 -0500

    Feeds: Grid - proof of concept
    Ref DSYS-2764

--- packages/feeds/source/feeds/experts/index.ts
+++ packages/feeds/source/feeds/experts/index.ts
@@ -20,5 +20,21 @@
  * document.querySelector('.container').appendChild(list.element);
  * ```
  * @since 1.17.0
+ * Expert Feeds
+ *
+ * Various layouts for displaying expert profiles.
+ *
+ * @module feeds/experts
+ */
+
+/**
+ * Grid layout for expert profiles
+ * Displays experts in a multi-column responsive grid
+ */
+export { default as grid } from './grid';
+
+/**
+ * List layout for expert profiles
+ * Displays experts in a single-column vertical list
  */
 export { default as list } from './list';

d638d6003d7d0f301a668550d5ab572941337785
Mei Lu <meilu@umd.edu>
2025-12-19 12:53:00 -0500

    Elements - Social Sharing element model conversion

---
+++ packages/elements/source/composite/social/_types.ts
@@ -0,0 +1,9 @@
+export type SocialSharingProps = {
+  isFixed?: boolean;
+  includeFacebook?: boolean;
+  includeTwitter?: boolean;
+  includeEmail?: boolean;
+  includePrint?: boolean;
+  title?: string | null;
+  url?: string | null;
+};

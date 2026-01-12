cb3817318c4814b8f74530850ef6ec4adca006e4
Mei Lu <meilu@umd.edu>
2026-01-12 15:09:36 -0500

    Components, Elements, Model - Quote Statement

--- packages/elements/source/composite/quote/elements/index.ts
+++ packages/elements/source/composite/quote/elements/index.ts
@@ -3,3 +3,8 @@ export { createCompositeQuoteText as text } from './text';
 export { createCompositeQuoteIcon as icon } from './icon';
 export { createCompositeQuoteAction as action } from './action';
 export { createCompositeQuote as quote } from './quote';
+export {
+  createStatementQuoteHeadline as statementHeadline,
+  createStatementQuoteAttribution as statementAttribution,
+} from './text';
+export { createStatementQuoteAction as statementAction } from './action';

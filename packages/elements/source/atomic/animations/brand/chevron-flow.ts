c4ef07590d290493b7cce21bfaeb75506254cb1b
Mei Lu <meilu@umd.edu>
2026-04-01 13:25:20 -0400

    Elements - Convert Animation Chevron Flow to Model Builder

--- packages/elements/source/atomic/animations/brand/chevron-flow.ts
+++ packages/elements/source/atomic/animations/brand/chevron-flow.ts
@@ -1,3 +1,5 @@
+import { ElementBuilder } from '@universityofmaryland/web-builder-library';
+
 type TypeAnimationProps = {
   container: HTMLDivElement;
   getHeightDiff: () => number;
@@ -5,18 +7,6 @@ type TypeAnimationProps = {
   completedCallback?: () => void;
 };
 
-type TypeArrowProps = TypeAnimationProps & {
-  id: string;
-  wrapperStyleRef?: string;
-};
-
-const ELEMENT_CONTAINER = 'animation-overlay-container';
-const ELEMENT_LAYER_OVERLAY_CONTAINER = 'animation-overlay-layer-container';
-const ELEMENT_LAYER_RED_ARROW_CONTAINER =
-  'animation-overlay-layer-red-arrow-container';
-const ELEMENT_LAYER_BLACK_ARROW_CONTAINER =
-  'animation-overlay-layer-black-arrow-container';
-const ELEMENT_GENERIC_ARROW = 'aninmation-arrow';
 
 const ID_OVERLAY_LEFT = 'animation-overlay-left';
 const ID_OVERLAY_RIGHT = 'animation-overlay-right';
@@ -24,141 +14,92 @@ const ID_SLIDE_IN_RED = 'slide-in-red-arrow';
 const ID_ARROW_BEHIND_OVERLAY = 'animation-overlay-arrow';
 const ID_ARROW_BEHIND_FOLLOWING = 'animation-following-arrow';
 const ID_ARROW_BEHIND_FOLLOWING_TWO = 'animation-following-arrow-two';
+const CLASS_ANIMATION_ARROW = 'animation-arrow';
 
 const TIMING_ANIMATION_ARROW_FIRST_TIME_IN = 1200;
 const TIMING_ANIMATION_ARROW_FIRST_TIME_OUT = 4500;
 
-const FollowingArrowStyles = `
-  .${ELEMENT_LAYER_BLACK_ARROW_CONTAINER} {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    transform: translate(-140%, -50%);
-    height: 100%;
-    z-index: 99;
-  }
-
-  .${ELEMENT_LAYER_BLACK_ARROW_CONTAINER} path {
-    fill: #000;
-  }
-
-  #${ID_ARROW_BEHIND_FOLLOWING} {
-    transform: translateX(-90%);
-  }
-`;
-
-const MainArrowStyles = `
-  .${ELEMENT_LAYER_RED_ARROW_CONTAINER} {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    transform: translate(-100%, -50%);
-    z-index: 999;
-    display: flex;
-    align-items: center;
-  }
-
-  .${ELEMENT_GENERIC_ARROW} {
-    height: 100%;
-  }
-`;
-
-const OverlayStyles = `
-  .${ELEMENT_LAYER_OVERLAY_CONTAINER} {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: auto;
-    height: auto;
-    min-width: 100%;
-    min-height: 100%;
-    z-index: 9;
-    display: flex;
-  }
-
-  #${ID_OVERLAY_RIGHT} {
-    width: 50%;
-  }
-
-  #${ID_OVERLAY_LEFT} {
-    width: 50%;
-  }
-
-  #${ID_ARROW_BEHIND_OVERLAY} {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    transform: translate(-60%, -50%);
-    height: 100%;
-    z-index: 9;
-    fill: #000;
-  }
-
-  #${ID_ARROW_BEHIND_OVERLAY} path {
-    fill: #000;
-  }
-`;
-
-const STYLES_ANIMATION_BRAND_OVERLAY_ELEMENT = `
-  ${OverlayStyles}
-  ${MainArrowStyles}
-  ${FollowingArrowStyles}
-
-  @media (prefers-reduced-motion: reduce) {
-    .${ELEMENT_CONTAINER} svg {
-      display: none;
-    }
-  }
-`;
-
-const CreateAnimatioOverlayContainer = (props: TypeAnimationProps) => {
-  const { container } = props;
-  const wrapper = document.createElement('div');
+const CreateAnimatioOverlayContainer = () => {
   const SVG_OVERLAY_LEFT = `<svg id="${ID_OVERLAY_LEFT}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="60 0 857.57 976"><defs><style>.overlay-left{fill:#000;fill-rule:evenodd;stroke-width:0px;}</style></defs><path class="overlay-left" d="M492.28,0H0v976h503.46l414.11-481.49L492.28,0Z"/></svg>`;
   const SVG_OVERLAY_RIGHT = `<svg id="${ID_OVERLAY_RIGHT}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 690.15 976"><defs><style>.overlay-right{fill:#000;fill-rule:evenodd;stroke-width:0px;}</style></defs><path class="overlay-right" d="M11.18,976l414.11-481.49L0,0h690.15v976H11.18Z"/></svg>`;
 
-  wrapper.classList.add(ELEMENT_LAYER_OVERLAY_CONTAINER);
-  wrapper.innerHTML = `${SVG_OVERLAY_LEFT} ${SVG_OVERLAY_RIGHT}`;
-  container.appendChild(wrapper);
-  return wrapper;
+  return new ElementBuilder()
+    .withClassName('animation-overlay-layer-container')
+    .withHTML(`${SVG_OVERLAY_LEFT} ${SVG_OVERLAY_RIGHT}`)
+    .withStyles({
+      element: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 'auto',
+        height: 'auto',
+        minWidth: '100%',
+        minHeight: '100%',
+        zIndex: '9',
+        display: 'flex',
+
+        [`& #${ID_OVERLAY_RIGHT}`]: { width: '50%' },
+        [`& #${ID_OVERLAY_LEFT}`]: { width: '50%' },
+        [`& #${ID_ARROW_BEHIND_OVERLAY}`]: {
+          position: 'absolute',
+          top: '50%',
+          left: '0',
+          transform: 'translate(-60%, -50%)',
+          height: '100%',
+          zIndex: '9',
+          fill: '#000',
+          '& path': { fill: '#000' },
+        },
+      },
+    })
+    .build();
 };
 
-const CreateAnimationArrowContainer = (props: TypeArrowProps) => {
-  const { container, id, wrapperStyleRef } = props;
-  const wrapper = document.createElement('div');
+const createArrowSvg = (id: string) =>
+  `<svg id="${id}" class="${CLASS_ANIMATION_ARROW}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 973.01 976"><defs><style>.arrow-video{fill:#e21833;fill-rule:evenodd;}</style></defs><path class="arrow-video" d="m427.44,497.01L0,0h545.57l427.44,497.01-411.96,478.99H15.48l411.96-478.99Z"/></svg>`;
 
-  const SVG_ANIMATION_ARROW = `<svg id="${id}" class="${ELEMENT_GENERIC_ARROW}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 973.01 976"><defs><style>.arrow-video{fill:#e21833;fill-rule:evenodd;}</style></defs><path class="arrow-video" d="m427.44,497.01L0,0h545.57l427.44,497.01-411.96,478.99H15.48l411.96-478.99Z"/></svg>`;
+const CreateOverlayArrow = (id: string) =>
+  new ElementBuilder().withHTML(createArrowSvg(id)).build();
 
-  if (wrapperStyleRef) {
-    wrapper.classList.add(wrapperStyleRef);
-  }
+const CreateRedArrow = (id: string) =>
+  new ElementBuilder()
+    .withClassName('animation-overlay-layer-red-arrow-container')
+    .withHTML(createArrowSvg(id))
+    .withStyles({
+      element: {
+        position: 'absolute',
+        top: '50%',
+        left: '0',
+        transform: 'translate(-100%, -50%)',
+        zIndex: '999',
+        display: 'flex',
+        alignItems: 'center',
+        `& .${CLASS_ANIMATION_ARROW}`: { height: '100%' },
+      },
+    })
+    .build();
 
-  wrapper.innerHTML = `${SVG_ANIMATION_ARROW}`;
-
-  container.appendChild(wrapper);
-
-  return wrapper;
-};
-
-const SizeElements = {
-  Arrow: ({ arrow, height }: { arrow: HTMLDivElement; height: number }) => {
-    arrow.style.height = `${height}px`;
-  },
-  Arrows: (props: TypeAnimationProps) => {
-    const { container, getContainerHeight } = props;
-    const height = getContainerHeight();
-
-    const ArrowContainers = Array.from(
-      container.querySelectorAll(`.${ELEMENT_GENERIC_ARROW}`),
-    ) as HTMLDivElement[];
-
-    ArrowContainers.forEach((arrow) => SizeElements.Arrow({ arrow, height }));
-  },
-  All: (props: TypeAnimationProps) => {
-    SizeElements.Arrows(props);
-  },
-};
+const CreateBlackArrow = (id: string) =>
+  new ElementBuilder()
+    .withClassName('animation-overlay-layer-black-arrow-container')
+    .withHTML(createArrowSvg(id))
+    .withStyles({
+      element: {
+        position: 'absolute',
+        top: '50%',
+        left: '0',
+        transform: 'translate(-140%, -50%)',
+        height: '100%',
+        zIndex: '99',
+        '& path': { fill: '#000' },
+        `& .${CLASS_ANIMATION_ARROW}`: { height: '100%' },
+        [`& #${ID_ARROW_BEHIND_FOLLOWING}`]: {
+          transform: 'translateX(-90%)',
+        },
+      },
+    })
+    .build();
 
 const AnimateFollowingArrows = (props: TypeAnimationProps) => {
   const { container } = props;
@@ -202,7 +143,7 @@ const AnimateMainArrow = (props: TypeAnimationProps) => {
     diff -
     Math.abs(overlayPosition.left);
 
-  SizeElements.Arrow({ arrow, height });
+  arrow.style.height = `${height}px`;
   arrow.style.transition = `transform ${TIMING_ANIMATION_ARROW_FIRST_TIME_IN}ms ease-in-out`;
   arrow.style.transform = `translateX(${alignment}px)`;
 
@@ -262,40 +203,42 @@ export const createAnimationChevronFlow = ({
   isAnimationOnLoad?: boolean;
 }) =>
   (() => {
-    const container = document.createElement('div');
     let windowWidth = window.innerWidth;
-    let styles = STYLES_ANIMATION_BRAND_OVERLAY_ELEMENT;
 
     const getContainerHeight = () => sizedContainer.offsetHeight;
-    const getHeightDiff = () => {
-      return sizedContainer.offsetHeight;
-    };
+    const getHeightDiff = () => sizedContainer.offsetHeight;
+
+    const containerBuilder = new ElementBuilder()
+      .withClassName('animation-overlay-container')
+      .withStyles({
+        element: {
+          '@media (prefers-reduced-motion: reduce)': {
+            '& svg': { display: 'none' },
+          },
+        },
+      });
+    const containerEl = containerBuilder.getElement() as HTMLDivElement;
+
     const data = {
-      container: container,
+      container: containerEl,
       getHeightDiff,
       getContainerHeight,
       completedCallback,
     };
-    const overlay = CreateAnimatioOverlayContainer(data);
-    const overlayArrow = CreateAnimationArrowContainer({
-      ...data,
-      id: ID_ARROW_BEHIND_OVERLAY,
-    });
-    const slideInArrow = CreateAnimationArrowContainer({
-      ...data,
-      id: ID_SLIDE_IN_RED,
-      wrapperStyleRef: ELEMENT_LAYER_RED_ARROW_CONTAINER,
-    });
-    const slideInArrowBlackOne = CreateAnimationArrowContainer({
-      ...data,
-      id: ID_ARROW_BEHIND_FOLLOWING,
-      wrapperStyleRef: ELEMENT_LAYER_BLACK_ARROW_CONTAINER,
-    });
-    const slideInArrowBlackTwo = CreateAnimationArrowContainer({
-      ...data,
-      id: ID_ARROW_BEHIND_FOLLOWING_TWO,
-      wrapperStyleRef: ELEMENT_LAYER_BLACK_ARROW_CONTAINER,
-    });
+
+    const overlay = CreateAnimatioOverlayContainer();
+    const overlayArrow = CreateOverlayArrow(ID_ARROW_BEHIND_OVERLAY);
+    const slideInArrow = CreateRedArrow(ID_SLIDE_IN_RED);
+    const slideInArrowBlackOne = CreateBlackArrow(ID_ARROW_BEHIND_FOLLOWING);
+    const slideInArrowBlackTwo = CreateBlackArrow(ID_ARROW_BEHIND_FOLLOWING_TWO);
+    const children = [
+      overlay,
+      overlayArrow,
+      slideInArrow,
+      slideInArrowBlackOne,
+      slideInArrowBlackTwo,
+    ];
+
     const EventLoad = () => {
       if (isAnimationOnLoad) {
         AnimationSequence(data);
@@ -303,7 +246,7 @@ export const createAnimationChevronFlow = ({
       }
 
       const scrollEvent = () => {
-        const containerPosition = container.getBoundingClientRect();
+        const containerPosition = containerEl.getBoundingClientRect();
         const top = containerPosition.top;
 
         if (top < window.innerHeight && window.scrollY !== 0) {
@@ -314,27 +257,18 @@ export const createAnimationChevronFlow = ({
 
       window.addEventListener('scroll', scrollEvent);
     };
+
     const EventResize = () => {
       if (windowWidth !== window.innerWidth) {
         window.removeEventListener('resize', EventResize);
-        container.remove();
+        containerEl.remove();
       }
     };
 
-    container.classList.add(ELEMENT_CONTAINER);
-    container.appendChild(overlay);
-    container.appendChild(overlayArrow);
-    container.appendChild(slideInArrow);
-    container.appendChild(slideInArrowBlackOne);
-    container.appendChild(slideInArrowBlackTwo);
-
     window.addEventListener('resize', EventResize);
 
-    return {
-      element: container,
-      styles,
-      events: {
-        load: EventLoad,
-      },
-    };
+    return containerBuilder
+      .withChildren(...children)
+      .withEvents({ load: EventLoad })
+      .build();
   })();

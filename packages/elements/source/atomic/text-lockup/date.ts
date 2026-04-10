eb6fdc448ec44e1baf7fd7a2d31a93aef279a780
Mei Lu <meilu@umd.edu>
2026-04-10 09:22:22 -0400

    Elements - Date text lockup conversion to ElementBuilder

--- packages/elements/source/atomic/text-lockup/date.ts
+++ packages/elements/source/atomic/text-lockup/date.ts
@@ -1,7 +1,7 @@
 import * as animation from '@universityofmaryland/web-styles-library/animation';
 import * as token from '@universityofmaryland/web-token-library';
 import * as typography from '@universityofmaryland/web-styles-library/typography';
-import { jssToCSS } from '@universityofmaryland/web-utilities-library/styles';
+import { ElementBuilder } from '@universityofmaryland/web-builder-library';
 import { truncateText } from '@universityofmaryland/web-utilities-library/string';
 import { wrapLinkForAnimation } from '@universityofmaryland/web-utilities-library/animation';
 
@@ -11,97 +11,65 @@ type TypeEventLockupDate = {
   isThemeDark?: boolean;
 };
 
-const ATTRIBUTE_THEME = 'theme';
-const THEME_DARK = 'dark';
-const IS_THEME_DARK = `[${ATTRIBUTE_THEME}="${THEME_DARK}"]`;
-
-const EVENT_DATE_CONTAINER = 'event-lockup-container';
-const EVENT_HEADLINE = 'event-lockup-headline';
-
-// prettier-ignore
-const HeadlineStyles = `
-  .${EVENT_HEADLINE} {
-    margin-left: ${token.spacing.sm};
-  }
-
-  ${jssToCSS({
-    styleObj: {
-      [`.${EVENT_HEADLINE}`]: typography.sans.large,
-    },
-  })}
-
-  ${jssToCSS({
-    styleObj: {
-      [`.${EVENT_HEADLINE} *`]: typography.sans.large,
-    },
-  })}
-
-  @media (max-width: 767px) {
-    .${EVENT_HEADLINE},
-    .${EVENT_HEADLINE} * {
-      font-size: ${token.font.size.sm};
-      font-weight: 600;
-    }
-  }
-
-  ${jssToCSS({
-    styleObj: {
-      [`.${EVENT_HEADLINE} a`]: animation.line.slideUnderBlack,
-    },
-  })}
-
-  ${jssToCSS({
-    styleObj: {
-      [`.${EVENT_DATE_CONTAINER}${IS_THEME_DARK} .${EVENT_HEADLINE} a`]: animation.line.slideUnderWhite,
-    },
-  })}
-
-  .${EVENT_DATE_CONTAINER}${IS_THEME_DARK} .${EVENT_HEADLINE},
-  .${EVENT_DATE_CONTAINER}${IS_THEME_DARK} .${EVENT_HEADLINE} * { 
-    color: ${token.color.white};
-  }
-`;
-
-// prettier-ignore
-const ComponentStyles = `
-  .${EVENT_DATE_CONTAINER} {
-    display: flex;
-    align-items: center;
-    text-align: left;
-  }
-  
-  .${EVENT_DATE_CONTAINER} * {
-    color: currentColor;
-  }
-
-
-  ${HeadlineStyles}
-`;
-
 export const createTextLockupDate = (props: TypeEventLockupDate) => {
   const { headline, dateSign, isThemeDark } = props;
-  const container = document.createElement('div');
-  let styles = ComponentStyles;
 
-  container.classList.add(EVENT_DATE_CONTAINER);
-  if (isThemeDark) container.setAttribute(ATTRIBUTE_THEME, THEME_DARK);
+  const createHeadline = () => {
+    if (!headline) {
+      console.warn('Headline element is required');
+      return;
+    }
 
-  if (dateSign) {
-    container.appendChild(dateSign.element);
-    styles += dateSign.styles;
-  }
-
-  if (headline) {
     const modifiedText = truncateText({
       text: headline.innerHTML,
       maxTextSize: 50,
     });
 
     headline.innerHTML = modifiedText;
-    headline.classList.add(EVENT_HEADLINE);
     wrapLinkForAnimation({ element: headline });
-    container.appendChild(headline);
-  }
 
-  return { element: container, styles };
+    return new ElementBuilder(headline)
+      .withClassName('event-lockup-headline')
+      .withStyles({
+        element: {
+          marginLeft: token.spacing.sm,
+          ...typography.sans.large,
+          ...(isThemeDark && { color: token.color.white }),
+          [`@media (${token.media.queries.large.max})`]: {
+            fontSize: token.font.size.sm,
+            fontWeight: 600,
+          },
+          '& *': {
+            ...typography.sans.large,
+            ...(isThemeDark && { color: token.color.white }),
+            [`@media (${token.media.queries.large.max})`]: {
+              fontSize: token.font.size.sm,
+              fontWeight: 600,
+            },
+          },
+          '& a': {
+            ...animation.line.slideUnderBlack,
+            ...(isThemeDark && animation.line.slideUnderWhite),
+          },
+        },
+      });
+  };
+
+  const headlineElement = createHeadline();
+  const children = [dateSign, headlineElement].filter(
+    (child) => child != null,
+  );
+
+  return new ElementBuilder()
+    .withClassName('event-lockup-container')
+    .withStyles({
+      element: {
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'left',
+        '& *': { color: 'currentColor' },
+      },
+    })
+    .withChildren(...children)
+    .build();
 };

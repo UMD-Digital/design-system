86aa280246cf5b5cfaba687fa5db31454474d3cd
Mei Lu <meilu@umd.edu>
2026-04-17 09:37:20 -0400

    Elements - Scrolltop refactor to element builder

--- packages/elements/source/composite/layout/scroll-top/index.ts
+++ packages/elements/source/composite/layout/scroll-top/index.ts
@@ -1,4 +1,5 @@
 import * as token from '@universityofmaryland/web-token-library';
+import { ElementBuilder } from '@universityofmaryland/web-builder-library';
 import { debounce } from '@universityofmaryland/web-utilities-library/performance';
 import { arrow_up as iconArrowUp } from '@universityofmaryland/web-icons-library/arrows';
 
@@ -6,67 +7,79 @@ type TypeScrollTopProps = {
   isFixed?: boolean;
 };
 
-const ELEMENT_NAME = 'umd-element-scroll-top';
-const ELEMENT_SCROLL_TOP_DECLARATION = 'scroll-top-declarion';
-const ELEMENT_SCROLL_TOP_CONTAINER = 'scroll-top-container';
-
-// prettier-ignore
-const STYLES_SCROLL_TOP_ELEMENT = `
-  .${ELEMENT_SCROLL_TOP_DECLARATION} {
-    container: ${ELEMENT_NAME} / inline-size;
-  }
-
-  .${ELEMENT_SCROLL_TOP_CONTAINER} {
-    background-color: ${token.color.black};
-    width: 100%;
-    height: 100%;
-    min-height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid ${token.color.gold};
-    cursor: pointer;
-    transition: transform 250ms ease-in-out;
-  }
-
-  .${ELEMENT_SCROLL_TOP_CONTAINER} svg {
-    display: block;
-    width: 14px;
-    height: 14px;
-  }
-`;
-
-// @media (${token.media.queries.medium.max}) {
-//   .${ELEMENT_SCROLL_TOP_CONTAINER} {
-//     display: none;
-//   }
-// }
-
 const CreateScrollTopElement = (props: TypeScrollTopProps) =>
   (() => {
+    const DEFAULT_STARTING_POSITION = `translateX(100vh)`;
     const { isFixed = false } = props;
-    const declaration = document.createElement('div');
-    const container = document.createElement('button');
-    const text = document.createElement('span');
-    const defaultStartingPosition = `translateX(100vh)`;
-    const getSpacing = () => (window.innerWidth > 768 ? 40 : 8);
+
     let startingPosition: null | number = null;
     let isShowing = false;
 
+    const textBuilder = new ElementBuilder('span')
+      .withClassName('sr-only')
+      .withHTML('Scroll To Top');
+
+    const containerBuilder = new ElementBuilder('button')
+      .withClassName('scroll-top-container')
+      .withHTML(iconArrowUp)
+      .withChild(textBuilder)
+      .withStyles({
+        element: {
+          backgroundColor: token.color.black,
+          width: '100%',
+          height: '100%',
+          minHeight: '40px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          border: `1px solid ${token.color.gold}`,
+          cursor: 'pointer',
+          transition: 'transform 250ms ease-in-out',
+          '& svg': {
+            display: 'block',
+            width: '14px',
+            height: '14px',
+          },
+        },
+      })
+      .on('click', () => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+      });
+
+    const declarationBuilder = new ElementBuilder()
+      .withClassName('scroll-top-declarion')
+      .withChild(containerBuilder)
+      .withStyles({
+        element: {
+          container: 'umd-element-scroll-top / inline-size',
+          width: '100%',
+          height: '100%',
+          ...(isFixed && {
+            width: '40px',
+            height: '40px',
+            position: 'absolute',
+            inset: '0',
+            margin: 'auto',
+          }),
+        },
+      });
+
+    const container = containerBuilder.getElement() as HTMLButtonElement;
+    const declaration = declarationBuilder.getElement() as HTMLDivElement;
+
     const eventResize = () => {
       startingPosition = declaration.getBoundingClientRect().right;
+
+      if (isFixed && !isShowing) {
+        setHiddenPosition();
+      }
     };
 
     const eventScroll = () => {
-      const windowHeight = window.innerHeight;
       const pagePosition = window.scrollY;
-      const isPastFirstView = pagePosition > windowHeight;
-      const isNearBottom =
-        pagePosition + windowHeight >=
-        document.body.scrollHeight - windowHeight;
-      const showWindow = isPastFirstView && !isNearBottom;
+      const isPastFirstView = pagePosition > 0;
 
-      if (showWindow) {
+      if (isPastFirstView) {
         setVisiblePosition();
       } else {
         setHiddenPosition();
@@ -81,69 +94,48 @@ const CreateScrollTopElement = (props: TypeScrollTopProps) =>
     };
 
     const setHiddenPosition = () => {
-      if (isShowing) {
-        isShowing = false;
-
-        if (startingPosition) {
-          const spacing = getSpacing();
-          const translateAmount = window.innerWidth - startingPosition;
-          const position = translateAmount + spacing + container.offsetWidth;
-
-          container.style.transform = `translateX(${position}px)`;
-        } else {
-          container.style.transform = defaultStartingPosition;
+      const getSpacing = () => {
+        if (window.innerWidth > 768) {
+          return 40;
         }
+
+        return 8;
+      };
+
+      isShowing = false;
+
+      if (startingPosition) {
+        const spacing = getSpacing();
+        const translateAmount = window.innerWidth - startingPosition;
+        const position = translateAmount + spacing + container.offsetWidth;
+
+        container.style.transform = `translateX(${position}px)`;
+      } else {
+        container.style.transform = DEFAULT_STARTING_POSITION;
       }
     };
 
     const load = () => {
       startingPosition = declaration.getBoundingClientRect().right;
       container.style.transition = 'none';
-      setHiddenPosition();
+
+      if (isFixed) {
+        eventScroll();
+      }
 
       setTimeout(() => {
         container.style.transition = 'transform 250ms ease-in-out';
       }, 100);
     };
 
-    text.classList.add('sr-only');
-    text.innerHTML = 'Scroll To Top';
-
-    container.classList.add(ELEMENT_SCROLL_TOP_CONTAINER);
-    container.addEventListener('click', () => {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-      });
-    });
-    container.innerHTML = iconArrowUp;
-    container.appendChild(text);
-
-    declaration.appendChild(container);
-    declaration.classList.add(ELEMENT_SCROLL_TOP_DECLARATION);
-
     if (isFixed) {
-      container.style.transform = defaultStartingPosition;
+      container.style.transform = DEFAULT_STARTING_POSITION;
 
-      window.addEventListener(
-        'scroll',
-        debounce(() => eventScroll(), 10),
-      );
-
-      window.addEventListener(
-        'resize',
-        debounce(() => eventResize(), 20),
-      );
+      window.addEventListener('scroll', debounce(eventScroll, 10));
+      window.addEventListener('resize', debounce(eventResize, 20));
     }
 
-    return {
-      element: declaration,
-      styles: STYLES_SCROLL_TOP_ELEMENT,
-      events: {
-        load,
-      },
-    };
+    return declarationBuilder.withEvents({ load }).build();
   })();
 
 export const createCompositeLayoutScrollTop = CreateScrollTopElement;

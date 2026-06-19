478f81049fcfe7215731df208633fe1af5eb89c0
Josh Magness <magnessjo@gmail.com>
2026-06-19 11:31:24 -0400

    Element - full screen button styles

--- packages/elements/source/atomic/buttons/fullscreen.ts
+++ packages/elements/source/atomic/buttons/fullscreen.ts
@@ -1,6 +1,9 @@
+import * as token from '@universityofmaryland/web-token-library';
+import * as typography from '@universityofmaryland/web-styles-library/typography';
 import { ElementBuilder } from '@universityofmaryland/web-builder-library';
-import * as Styles from '@universityofmaryland/web-styles-library';
 import { fullscreen as iconFullscreen } from '@universityofmaryland/web-icons-library/controls';
+
+const CLASS_NAME = 'umd-action-button-full-screen';
 
 const create = ({
   callback,
@@ -11,14 +14,49 @@ const create = ({
 }) =>
   (() => {
     return new ElementBuilder('button')
+      .withClassName(CLASS_NAME)
       .withAttribute('data-index', index.toString())
       .withAttribute('aria-label', 'View Full Screen')
       .withHTML(`Full Screen <span></span>${iconFullscreen}`)
       .on('click', () => callback(index))
-      .styled(Styles.element.action.button.fullScreen)
+      .styled(typography.sans.min)
+      .withStyles({
+        element: {
+          position: 'absolute',
+          top: '0',
+          right: '0',
+          color: token.color.white,
+          textTransform: 'uppercase',
+          fontWeight: '700',
+          padding: token.spacing.min,
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          transition: 'background-color 0.3s',
+
+          '&:focus': {
+            backgroundColor: 'rgba(0, 0, 0, 1)',
+          },
+
+          '@media (hover: hover)': {
+            '&:hover': {
+              backgroundColor: 'rgba(0, 0, 0, 1)',
+            },
+          },
+
+          '& > span': {
+            display: 'block',
+            height: '12px',
+            width: '1px',
+            margin: `0 ${token.spacing.min}`,
+            backgroundColor: token.color.gray.mediumAA,
+          },
+        },
+      })
       .build();
   })();
 
 export const fullscreen = {
   create,
+  className: CLASS_NAME,
 };

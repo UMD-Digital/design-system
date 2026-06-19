50112da4e766b683c255209b581ca961e9996bc6
Josh Magness <magnessjo@gmail.com>
2026-06-19 11:31:07 -0400

    Styles - Remove CSS for full screen button

--- packages/styles/source/element/action/button.ts
+++ packages/styles/source/element/action/button.ts
@@ -3,56 +3,12 @@
  * Provides specialized button styles for multimedia and UI controls.
  */
 
-import { color, media, spacing } from '@universityofmaryland/web-token-library';
-import { sans } from '../../typography';
+import { color, media } from '@universityofmaryland/web-token-library';
 import { create } from '../../utilities';
 import type { JssObject } from '../../_types';
 
 // Consistent naming
 const classNamePrefix = 'umd-action-button';
-
-/**
- * Fullscreen button style.
- * @returns {JssObject} Button style for fullscreen controls.
- * @example
- * ```typescript
- * import * as Styles from '@universityofmaryland/web-styles-library';
- * Styles.element.action.button.fullScreen
- * ```
- * @example
- * ```css
- * class="umd-action-button-full-screen"
- * ```
- * @since 1.1.0
- */
-const { className: _, ...sansMinStyles } = sans.min;
-export const fullScreen: JssObject = create.jss.objectWithClassName({
-  className: `${classNamePrefix}-full-screen`,
-  ...sansMinStyles,
-  position: 'absolute',
-  top: '0',
-  right: '0',
-  color: `${color.white}`,
-  textTransform: 'uppercase',
-  fontWeight: '700',
-  padding: `${spacing.min}`,
-  display: `flex`,
-  alignItems: `center`,
-  backgroundColor: `rgba(0, 0, 0, 0.5)`,
-  transition: `background-color 0.3s`,
-
-  [`&:hover, &:focus`]: {
-    backgroundColor: `rgba(0, 0, 0, 1)`,
-  },
-
-  '& > span': {
-    display: `block`,
-    height: `12px`,
-    width: `1px`,
-    margin: `0 ${spacing.min}`,
-    backgroundColor: `${color.gray.mediumAA}`,
-  },
-});
 
 /**
  * Video playback control button style.
